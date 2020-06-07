939c491f3ce8d6bc80e43c000aa445e4f83b9b5d
Brian Adams <adamsb@msu.edu>
2020-06-07 13:56:40 -0400

    fix(backend): remove reserved words from notifications table
    Cake does not quote identifiers by default so using reserved words
    as column names will result in a syntax error.  Changing Cake to quote
    identifiers is possible but comes with a modest
    performance hit.

--- tests/Fixture/NotificationsFixture.php
+++ tests/Fixture/NotificationsFixture.php
@@ -10,36 +10,7 @@ use Cake\TestSuite\Fixture\TestFixture;
  */
 class NotificationsFixture extends TestFixture
 {
-    /**
-     * Fields
-     *
-     * @var array
-     */
-    // @codingStandardsIgnoreStart
-    public $fields = [
-        'id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'autoIncrement' => true, 'precision' => null],
-        'fromuser' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => true, 'default' => null, 'comment' => '', 'precision' => null, 'autoIncrement' => null],
-        'touser' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => true, 'default' => null, 'comment' => '', 'precision' => null, 'autoIncrement' => null],
-        'content' => ['type' => 'text', 'length' => null, 'null' => true, 'default' => null, 'collate' => 'latin1_swedish_ci', 'comment' => '', 'precision' => null],
-        'created' => ['type' => 'datetime', 'length' => null, 'precision' => null, 'null' => false, 'default' => null, 'comment' => ''],
-        'modified' => ['type' => 'datetime', 'length' => null, 'precision' => null, 'null' => false, 'default' => null, 'comment' => ''],
-        'read' => ['type' => 'integer', 'length' => 1, 'unsigned' => false, 'null' => false, 'default' => '0', 'comment' => '', 'precision' => null, 'autoIncrement' => null],
-        'archived' => ['type' => 'integer', 'length' => 1, 'unsigned' => false, 'null' => false, 'default' => '0', 'comment' => '', 'precision' => null, 'autoIncrement' => null],
-        '_indexes' => [
-            'fromuser' => ['type' => 'index', 'columns' => ['fromuser'], 'length' => []],
-            'touser' => ['type' => 'index', 'columns' => ['touser'], 'length' => []],
-        ],
-        '_constraints' => [
-            'primary' => ['type' => 'primary', 'columns' => ['id'], 'length' => []],
-            'notifications_ibfk_1' => ['type' => 'foreign', 'columns' => ['fromuser'], 'references' => ['users', 'id'], 'update' => 'noAction', 'delete' => 'setNull', 'length' => []],
-            'notifications_ibfk_2' => ['type' => 'foreign', 'columns' => ['touser'], 'references' => ['users', 'id'], 'update' => 'noAction', 'delete' => 'setNull', 'length' => []],
-        ],
-        '_options' => [
-            'engine' => 'InnoDB',
-            'collation' => 'latin1_swedish_ci'
-        ],
-    ];
-    // @codingStandardsIgnoreEnd
+    public $import = ['table' => 'notifications'];
     /**
      * Init method
      *
@@ -50,13 +21,13 @@ class NotificationsFixture extends TestFixture
         $this->records = [
             [
                 'id' => 1,
-                'fromuser' => 1,
-                'touser' => 1,
-                'content' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
+                'from_user' => 1,
+                'to_user' => 1,
+                'content' => 'Test Notification',
                 'created' => '2020-02-25 18:15:21',
                 'modified' => '2020-02-25 18:15:21',
-                'read' => 1,
-                'archived' => 1,
+                'is_read' => 0,
+                'is_archived' => 0,
             ],
         ];
         parent::init();
