5b85d07044617f3451b1ba51fbaf0618d1e9b6e6
Brian Adams <adamsb@msu.edu>
2020-12-26 00:23:40 -0500

    docs: create skeleton setup for docs

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    title: 'CCR Documentation',
+    description: 'Technical and User documentation for the CCR project.',
+    themeConfig: {
+        sidebar: [
+          '/'
+        ],
+        repo: 'MESH-Research/CCR',
+        docsDir: 'docs',
+        editLinkText: 'Help us improve this page!',
+        editLinks: true
+      }
+  }
