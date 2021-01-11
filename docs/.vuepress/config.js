f6c6a0001ea97f4ba35df2716d3488c5ca8446dd
Brian Adams <adamsb@msu.edu>
2021-01-11 18:31:11 -0500

    Finishing docs framework

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,13 +1,67 @@
+const { description } = require('../package')
+
 module.exports = {
-    title: 'CCR Documentation',
-    description: 'Technical and User documentation for the CCR project.',
-    themeConfig: {
-        sidebar: [
-          '/'
-        ],
-        repo: 'MESH-Research/CCR',
-        docsDir: 'docs',
-        editLinkText: 'Help us improve this page!',
-        editLinks: true
+  title: 'Collaborative Community Review',
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+  base: process.env.BASE_URL || '/',
+  themeConfig: {
+    repo: 'https://github.com/MESH-Research/CCR',
+    branch: process.env.CURRENT_BRANCH,
+    editLinks: true,
+    docsDir: 'docs',
+    docsBranch: 'development',
+    editLinkText: '',
+    lastUpdated: true,
+    nav: [
+      {
+        text: 'Guide',
+        link: '/guide/',
+      },
+      {
+        text: 'Installation',
+        link: '/install/'
+      },
+      {
+        text: 'Contributing',
+        link: '/contributing/'
       }
-  }
+    ],
+    sidebar: {
+      '/guide/': [
+        {
+          title: 'Guide',
+          collapsable: false,
+        }
+      ],
+      '/install/': [
+        {
+          title: 'Installation',
+          collapsable: false,
+        }
+      ],
+      '/contributing/': [
+        {
+          title: 'Contributing',
+          collapsable: false,
+        }
+      ],
+    }
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
+    ['robots', {
+      host: "https://mesh-research.github.io/CCR",
+      disallowAll: true,
+    }
+    ]
+  ]
+}
---
+++ docs/index.md
@@ -0,0 +1,26 @@
+---
+home: true
+heroImage: /splash.png
+tagline: Documentation
+actionText: Quick Start →
+actionLink: /install/
+---
+# About
+
+In here should go a nice description about CCR and what it's intended to do.  
+
+
+
+::: slot home-banner
+
+::: danger HEADS UP
+  CCR is currently in the initial stages of development and isn't ready for deployment or testing just yet.  
+  Check out our  [contribution guidelines](https://github.com/MESH-Research/CCR/blob/master/.github/CONTRIBUTING.md) to help make CCR a reality.
+:::
+
+
+
+
+::: slot home-footer
+Developed by [MESH-Research](https://meshresearch.net/).
+:::
