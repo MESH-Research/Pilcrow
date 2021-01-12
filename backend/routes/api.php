5f21d7dc08f1054c399088509a10b442dc1ba619
gmeben <gmeben@gmail.com>
2021-01-12 11:20:39 -0500

    feat: Add disabled routes for testing

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -14,6 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Route::get('/register', [AuthController::class, 'register']);
+
+// Route::get('/login', [AuthController::class, 'login']);
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
