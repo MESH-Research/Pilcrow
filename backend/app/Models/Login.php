20207b7ff4973e891b98be528e96e3b50d6c880b
gmeben <gmeben@gmail.com>
2021-01-12 16:54:47 -0500

    feat: Add Login and Logout models

---
+++ backend/app/Models/Login.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class Login
+{
+    /**
+     * @param  null  $_
+     * @param  array<string, mixed>  $args
+     */
+    public function __invoke($_, array $args): User
+    {
+        // Plain Laravel: Auth::guard()
+        // Laravel Sanctum: Auth::guard(config('sanctum.guard', 'web'))
+        $guard = Auth::guard(config('sanctum.guard', 'web'));
+
+        if( ! $guard->attempt($args)) {
+            throw new \Error('Invalid credentials.');
+        }
+
+        /**
+         * Since we successfully logged in, this can no longer be `null`.
+         *
+         * @var \App\Models\User $user
+         */
+        $user = $guard->user();
+
+        return $user;
+    }
+}
---
+++ backend/app/Models/Logout.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class Logout
+{
+    /**
+     * @param  null  $_
+     * @param  array<string, mixed>  $args
+     */
+    public function __invoke($_, array $args): ? User
+    {
+        // Plain Laravel: Auth::guard()
+        // Laravel Sanctum: Auth::guard(config('sanctum.guard', 'web'))
+        $guard = Auth::guard(config('sanctum.guard', 'web'));
+
+        /** @var \App\Models\User|null $user */
+        $user = $guard->user();
+        $guard->logout();
+
+        return $user;
+    }
+}
