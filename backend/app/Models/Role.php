e5af2830772fdc4ea834f570f63c202bff4c9cb1
gmeben <gmeben@gmail.com>
2021-01-27 17:04:20 -0500

    feat: Add constants for role names

--- backend/app/Models/Role.php
+++ backend/app/Models/Role.php
@@ -8,4 +8,12 @@ use Spatie\Permission\Models\Role as ParentModel;
 class Role extends ParentModel
 {
     use HasFactory;
+
+    const APPLICATION_ADMINISTRATOR = 'Application Administrator';
+    const PUBLICATION_ADMINISTRATOR = 'Publication Administrator';
+    const EDITOR = 'Editor';
+    const REVIEW_COORDINATOR = 'Review Coordinator';
+    const REVIEWER = 'Reviewer';
+    const SUBMITTER = 'Submitter';
+
 }
