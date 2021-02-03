066c724be094125f1fcb54d8772c05a62489b5fe
Grant Eben <gmeben@users.noreply.github.com>
2021-02-03 14:27:44 -0500

    feat: extend graphql with user roles (#123)
    * Extend the GraphQL endpoint with roles
    * Add tests for querying roles from the GraphQL endpoint

--- backend/tests/Feature/UserPermissionsTest.php
+++ backend/tests/Feature/UserPermissionsTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use Nuwave\Lighthouse\Testing\MakesGraphQLRequests;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -11,7 +12,7 @@ use App\Models\Permission;
 
 class UserPermissionsTest extends TestCase
 {
-    use RefreshDatabase;
+    use MakesGraphQLRequests, RefreshDatabase;
 
     private $test_permission = 'test permission';
     private $test_user_role = 'Test User Role';
@@ -125,5 +126,57 @@ class UserPermissionsTest extends TestCase
         $test_role->givePermissionTo($this->test_permission);
         $user->assignRole(Role::PUBLICATION_ADMINISTRATOR);
         $this->assertFalse($user->can($this->test_permission));
+    }
+
+    /**
+     * @return void
+     */
+    public function testRoleForUserIsQueryableFromGraphqlEndpoint()
+    {
+        $test_role = Role::factory()->create([
+            'name' => $this->test_user_role
+        ]);
+        $user = User::factory()->create();
+        $user->assignRole($this->test_user_role);
+        $response = $this->graphQL(
+            'query getUser($id: ID) {
+                user(id: $id) {
+                    id
+                    name
+                    roles {
+                        id
+                        name
+                    }
+                }
+            }', ['id' => $user->id]
+        );
+        $expected_array = [
+            0 => [
+                'id' => (string) $test_role->id,
+                'name' => 'Test User Role'
+            ]
+        ];
+        $response->assertJsonPath("data.user.roles", $expected_array);
+    }
+
+    /**
+     * @return void
+     */
+    public function testUserWithNoRoleReturnsAnEmptyArrayWhenQueriedFromGraphqlEndpoint()
+    {
+        $user = User::factory()->create();
+        $response = $this->graphQL(
+            'query getUser($id: ID) {
+                user(id: $id) {
+                    id
+                    name
+                    roles {
+                        id
+                        name
+                    }
+                }
+            }', ['id' => $user->id]
+        );
+        $response->assertJsonPath("data.user.roles", []);
     }
 }
