f2cf722ebd709595fbbe976f856730fdb8e36b31
Brian Adams <adamsb@msu.edu>
2021-02-04 03:40:01 -0500

    Add comments to cypress custom commands

--- client/test/cypress/support/commands.js
+++ client/test/cypress/support/commands.js
@@ -24,10 +24,21 @@
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
+/**
+ * Custom command to select DOM element by data-cy attribute.
+ *
+ * see https://docs.cypress.io/guides/references/best-practices.html#Selecting-Elements
+ *
+ * @example cy.dataCy('greeting')
+ */
 Cypress.Commands.add("dataCy", value => {
   return cy.get(`[data-cy=${value}]`);
 });
 
+/**
+ * Custom command to test being on a given route.
+ * @example cy.testRoute('home')
+ */
 Cypress.Commands.add("testRoute", route => {
   cy.location().should(loc => {
     expect(loc.hash).to.contain(route);
@@ -37,18 +48,30 @@ Cypress.Commands.add("testRoute", route => {
 // these two commands let you persist local storage between tests
 const LOCAL_STORAGE_MEMORY = {};
 
+/**
+ * Persist current local storage data.
+ * @example cy.saveLocalStorage()
+ */
 Cypress.Commands.add("saveLocalStorage", () => {
   Object.keys(localStorage).forEach(key => {
     LOCAL_STORAGE_MEMORY[key] = localStorage[key];
   });
 });
 
+/**
+ * Restore saved data to local storage.
+ * @example cy.restoreLocalStorage()
+ */
 Cypress.Commands.add("restoreLocalStorage", () => {
   Object.keys(LOCAL_STORAGE_MEMORY).forEach(key => {
     localStorage.setItem(key, LOCAL_STORAGE_MEMORY[key]);
   });
 });
 
+/**
+ * Get the root vue object.
+ * @example cy.getVue()
+ */
 Cypress.Commands.add("getVue", () => {
   cy.get("#q-app").then($app => {
     cy.wrap($app.get(0).__vue__);
