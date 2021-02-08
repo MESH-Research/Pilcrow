34a2958b8d0c9755f111425696aa9ddcac6ab381
gmeben <gmeben@gmail.com>
2021-02-08 17:35:48 -0500

    feat: Add permission migration

---
+++ backend/database/migrations/2021_02_05_195435_add_first_permission.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class AddFirstPermission extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $guard_name = config('auth.defaults.guard');
+
+        DB::table('permissions')->insert([
+            'name' => 'reset passwords of other users',
+            'guard_name' => $guard_name,
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s')
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::disableForeignKeyConstraints();
+        DB::table('permissions')->truncate();
+        Schema::enableForeignKeyConstraints();
+    }
+}
