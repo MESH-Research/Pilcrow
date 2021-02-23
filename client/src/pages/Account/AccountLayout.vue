874e5d1def1d2c40658366818d6e1e2e83f09f8b
gmeben <gmeben@gmail.com>
2021-02-23 10:45:28 -0500

    feat: Use 2-column layout for profile section in AccountLayout

--- client/src/pages/Account/AccountLayout.vue
+++ client/src/pages/Account/AccountLayout.vue
@@ -2,15 +2,24 @@
   <q-page class="">
     <div class="row justify-center items-start content-start">
       <q-card class="col-sm-3 col-xs-12 no-shadow no-border-radius">
-        <q-card-section class="flex flex-center bg-blue-1">
+        <div class="row">
+        <q-card-section class="col-sm-12 col-xs-4 flex flex-center bg-blue-1">
           <avatar-block
             avatar-size="80px"
             :user="currentUser"
             class="text-center"
           />
         </q-card-section>
-        <q-card-section class="q-pa-none">
+        <q-card-section class="col-sm-12 col-xs-8 q-pa-none">
           <q-list>
+            <q-item
+              clickable
+              class="text-center bg-grey-2"
+            >
+              <q-item-section>
+                View Public Profile
+              </q-item-section>
+            </q-item>
             <q-item>
               <q-item-section>
                 Submissions Created
@@ -33,16 +42,9 @@
                 ??
               </q-item-section>
             </q-item>
-            <q-item
-              clickable
-              class="text-center bg-blue-1"
-            >
-              <q-item-section>
-                View Public Profile
-              </q-item-section>
-            </q-item>
           </q-list>
         </q-card-section>
+        </div>
       </q-card>
       <q-card class="col-sm-9 col-xs-12 no-shadow outline no-border-radius">
         <q-card-section class="q-pa-none">
