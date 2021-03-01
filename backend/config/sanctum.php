3cb26ad0a809fe36962edb4e3f1aa1da6910f639
Brian Adams <adamsb@msu.edu>
2021-03-01 13:43:34 -0500

    chore: create deployment dockerfiles for preview builds (#152)
    Co-authored-by: gmeben <gmeben@gmail.com>

--- backend/config/sanctum.php
+++ backend/config/sanctum.php
@@ -13,10 +13,7 @@ return [
     |
     */
 
-    'stateful' => explode(',', env(
-        'SANCTUM_STATEFUL_DOMAINS',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1'
-    )),
+    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', env('APP_HOSTNAME', 'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1'))),
 
     /*
     |--------------------------------------------------------------------------
