c0c685029e561a575b75e0f686825c1a38ffd0d1
Brian Adams <adamsb@msu.edu>
2021-03-02 18:17:46 -0500

    tests: add integration tests for login and register pages

--- client/test/cypress/integration/login.spec.js
+++ client/test/cypress/integration/login.spec.js
@@ -9,17 +9,42 @@ describe('login page', () => {
     it('allows a user to login', () => {
         cy.visit('/login');
         cy.get('.q-form').within(() => {
-            cy.dataCy('emailField').type('regularuser@ccrproject.dev');
-            cy.dataCy('passwordField').type('regularPassword!@#');
+            cy.dataCy('email_field').type('regularuser@ccrproject.dev');
+            cy.dataCy('password_field').type('regularPassword!@#');
+            cy.get('.q-card__actions').contains('Login').click();
+            cy.url().should('include', '/dashboard');
         });
-        cy.contains('Login').click();
     });
 
-    it('validates fields', () => {
+    it('validates fields and displays errors', () => {
+        cy.visit('/login');
+        cy.get('.q-form').within(() => {
+            cy.dataCy('email_field')
+                .type('{enter}')
+                .parents('label')
+                .should('have.class', 'q-field--error')
+            cy.dataCy('email_field').type('regularuser@ccrproject.dev{enter}');
 
+            cy.dataCy('password_field')
+                .parents('label')
+                .should('have.class', 'q-field--error')
+                .type('somePass{enter}');
+            
+            cy.dataCy('authFailureMessages')
+                .should('be.visible')
+                .contains('invalid credentials');
+        });
     });
 
     it('redirects to login when requesting a protected page', () => {
-        
+        cy.visit('/account/profile');
+        cy.url().should('include', '/login');
+        cy.get('[role="alert"]').contains('login to access that page');
+
+        cy.get('.q-form').within(() => {
+            cy.dataCy('email_field').type('regularuser@ccrproject.dev');
+            cy.dataCy('password_field').type('regularPassword!@#{enter}');
+            cy.url().should('include', '/account/profile');
+        })
     });
 });
--- client/test/cypress/integration/register.spec.js
+++ client/test/cypress/integration/register.spec.js
@@ -4,28 +4,87 @@
 
 describe("Register", () => {
     beforeEach(() => {
+        cy.task('resetDb');
         cy.visit("/register");
     });
 
-    it ("requires username", () => {
+    it("validates fields", () => {
+        cy.get('.q-form').within(() => {
+            cy.dataCy('email_field')
+                .type('{enter}')
+                .parents('label')
+                .should('have.class', 'q-field--error')
+            cy.dataCy('email_field')
+                .type('regularuser@ccrproject.dev{enter}')
+                .parents('label')
+                .should('have.class', 'q-field--error')
+                .contains('already registered');
+            
+            cy.dataCy('email_field')
+                .clear()
+                .type('ccrproject{enter}')
+                .parents('label')
+                .should('have.class', 'q-field--error')
+                .contains("a valid email");
+            
+            cy.dataCy('email_field')
+                .type('@ccrproject.dev{enter}')
+                .parents('label')
+                .should('not.have.class', 'q-field--error');
+            
+            
+            cy.dataCy('username_field')
+                .parents('label')
+                .should('have.class', 'q-field--error')
+                .contains('is required');
+            
+            cy.dataCy('username_field')
+                .type('regularUser{enter}')
+                .parents('label')
+                .should('have.class', 'q-field--error')
+                .contains('is not available');
 
+            cy.dataCy('username_field')
+                .type('newUser{enter}')
+                .parents('label')
+                .should('not.have.class', 'q-field--error');
+            
+            cy.dataCy('password_field')
+                .parents('label')
+                .should('have.class', 'q-field--error')
+                .contains('is required');
+            
+            cy.dataCy('password_field')
+                .type('password')
+                .parents('label')
+                .should('have.class', 'q-field--error')
+                .contains('be more complex');
+            
+            cy.dataCy('password_field')
+                .type('!@#$#@password')
+                .parents('label')
+                .should('not.have.class', 'q-field--error')
+            
+            cy.get('[type="submit"]').click();
+
+            cy.url().should('include', '/dashboard');
+
+
+        });
     });
 
-    it ("requires valid email", () => {
+    it('submits with enter key', () => {
+        cy.get('.q-form').within(() => {
+            cy.dataCy('username_field')
+                .type('newUserName');
+            cy.dataCy('email_field')
+                .type('newEmail@ccrproject.dev');
+            cy.dataCy('password_field')
+                .type('password_field!@#12{enter}');
+        });
 
-    });
-
-    it("requires valid password", () => {
-
-    });
-
-    it("registers a user", () => {
-
-    });
-
-    it("redirects to login on success", () => {
-
-    });
+        cy.url().should('include', '/dashboard');
+    })
 
 
 
