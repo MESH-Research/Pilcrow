0c91c8f2a9dc1156cbea8f9a69c0a2efd0b52919
gmeben <gmeben@gmail.com>
2021-03-02 18:24:05 -0500

    test: Add failing commented tests for email and password

--- client/test/cypress/integration/account.spec.js
+++ client/test/cypress/integration/account.spec.js
@@ -5,27 +5,37 @@ describe('Account', () => {
   beforeEach(() => {
     cy.task('resetDb');
     cy.login({ email: 'regularuser@ccrproject.dev' });
-    cy.visit('/');
+    cy.visit('/account/profile');
   });
 
   it('can visit the account profile page from the dropdown', () => {
+    cy.visit('/');
     cy.dataCy('dropdown_username').click();
     cy.dataCy('link_my_account').click();
     cy.url().should('include', '/account/profile');
   });
 
   it('can update the name field', () => {
-    cy.visit('/account/profile');
     cy.dataCy('update_user_name').clear().type('Updated User');
     cy.dataCy('update_user_button_save').click();
     cy.dataCy('avatar_name').contains('Updated User');
   });
 
   it('can update the username field', () => {
-    cy.visit('/account/profile');
     cy.dataCy('update_user_username').clear().type('updatedUser');
     cy.dataCy('update_user_button_save').click();
     cy.dataCy('avatar_username').contains('updatedUser');
   });
+
+  // TODO: Get these tests working
+  // it('can update the email field', () => {
+  //   cy.dataCy('update_user_email').clear().type('updateduser@ccrproject.dev');
+  //   cy.dataCy('update_user_button_save').click();
+  // });
+
+  // it('can update the password field', () => {
+  //   cy.dataCy('update_user_password').clear().type('XMYeygtC7TuxgER4');
+  //   cy.dataCy('update_user_button_save').click();
+  // });
 
 });
