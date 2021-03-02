1b20954a50a5b8a7e755e56b9a268c2be8d919be
gmeben <gmeben@gmail.com>
2021-03-02 11:11:39 -0500

    fix: Cypress test for accessing account profile page

--- client/test/cypress/integration/account.spec.js
+++ client/test/cypress/integration/account.spec.js
@@ -3,19 +3,15 @@
 
 describe("Account", () => {
   beforeEach(() => {
-    cy.visit("/");
-    cy.dataCy("link_register").click();
-    cy.dataCy("register_email").type("testemail2@email.com");
-    cy.dataCy("register_username").type("testusername2");
-    cy.dataCy("password").type("Ek-]'&7wD9,7&p{z");
-    cy.dataCy("button_register").click();
+    cy.task('resetDb');
+    cy.login({ email: "regularuser@ccrproject.dev" });
+    cy.visit('/');
+  });
 
-    cy.dataCy("login_email").type("testemail2@email.com");
-    cy.dataCy("password").type("Ek-]'&7wD9,7&p{z");
-    cy.dataCy("button_login").click();
-
+  it("can visit the account profile page from the dropdown", () => {
     cy.dataCy("dropdown_username").click();
     cy.dataCy("link_my_account").click();
+    cy.url().should("include", "/account/profile");
   });
 
   it("has authenticated after registering", () => {
