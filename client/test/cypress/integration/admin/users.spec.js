5d865d0dea071a17bd47bbb9b6b15de1ebc2d36e
Sruthin <gaddamsr@msu.edu>
2021-03-15 13:08:28 -0400

    test: Add a11y test for user index page

--- client/test/cypress/integration/admin/users.spec.js
+++ client/test/cypress/integration/admin/users.spec.js
@@ -1,5 +1,9 @@
 /// <reference types="Cypress" />
 /// <reference path="../../support/index.d.ts" />
+// Use `cy.dataCy` custom command for more robust tests
+// See https://docs.cypress.io/guides/references/best-practices.html#Selecting-Elements
+
+import 'cypress-axe';
 
 describe('Admin Users Index', () => {
   beforeEach(() => {
@@ -10,10 +14,15 @@ describe('Admin Users Index', () => {
     cy.login({ email: "regularuser@ccrproject.dev" });
     cy.visit('/admin/users');
     cy.url().should('include', '/error403');
-
+  });
+  it('allows access based on role and check for a11y issues', () => {
     cy.login({ email: "applicationadministrator@ccrproject.dev" });
     cy.visit('/admin/users');
     cy.url().should('not.include', '/error403');
-    cy.contains('admin users index');
+    // Inject the axe-core libraray
+    cy.injectAxe();
+    //cy.contains('admin users index');
+    cy.checkA11y();
   });
+
 });
