c23f5015f7c26089186d8f4400080d9d2c3d6130
gmeben <gmeben@gmail.com>
2021-03-18 15:36:42 -0400

    feat: Establish publication and user relationship

--- backend/app/Models/Publication.php
+++ backend/app/Models/Publication.php
@@ -12,4 +12,13 @@ class Publication extends Model
     protected $fillable = [
         'name'
     ];
+
+    /**
+     * Users that belong to a publication
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'publication_users');
+    }
 }
