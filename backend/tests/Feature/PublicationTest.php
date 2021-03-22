ed3ff84b573fe9214b7c312ff5b407e6b894483b
gmeben <gmeben@gmail.com>
2021-03-22 15:49:13 -0400

    test: Add tests for unassignment

--- backend/tests/Feature/PublicationTest.php
+++ backend/tests/Feature/PublicationTest.php
@@ -12,19 +12,41 @@ class PublicationTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function testThatPublicationsCanBeAssignedToAndQueriedForAUser()
+    public function testPublicationsCanBeAssignedToAndQueriedForAUser()
     {
         $publication = Publication::factory()->create();
         $user = User::factory()->create();
-        $user->publications()->attach([$publication->id]);
+        $user->publications()->attach($publication->id);
         $this->assertEquals($user->publications->first()->id, $publication->id);
+        $this->assertEquals($user->publications->count(), 1);
     }
 
-    public function testThatUsersCanBeAssignedToAndQueriedForAPublication()
+    public function testUsersCanBeAssignedToAndQueriedForAPublication()
     {
         $publication = Publication::factory()->create();
         $user = User::factory()->create();
         $publication->users()->attach([$user->id]);
         $this->assertEquals($publication->users->first()->id, $user->id);
+        $this->assertEquals($publication->users->count(), 1);
+    }
+
+    public function testUsersCanBeUnAssignedToAndQueriedForAPublication()
+    {
+        $publication = Publication::factory()->create();
+        $user = User::factory()->create();
+        $publication->users()->attach([$user->id]);
+        $publication->users()->detach([$user->id]);
+        $this->assertEmpty($publication->users->toArray());
+        $this->assertEquals($publication->users->count(), 0);
+    }
+
+    public function testPublicationsCanBeUnAssignedToAndQueriedForAUser()
+    {
+        $publication = Publication::factory()->create();
+        $user = User::factory()->create();
+        $user->publications()->attach([$user->id]);
+        $user->publications()->detach([$user->id]);
+        $this->assertEmpty($user->publications->toArray());
+        $this->assertEquals($user->publications->count(), 0);
     }
 }
