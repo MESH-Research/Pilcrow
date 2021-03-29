0ee4afcf126c3113f30be7fc2908b252ed297086
Grant Eben <gmeben@users.noreply.github.com>
2021-03-29 11:57:26 -0400

    feat: add publications (#187)
    * feat: Add Publication model, seeder, and factory
    
    * feat: Establish publication and user relationship
    
    * fix: Add default name to PublicationFactory
    
    * test: Add assignment and query tests
    
    * test: Add tests for unassignment
    
    * refactor: Remove unnecessary seeder
    
    * style: Fix linting errors
    
    * fix: Unused variable in test
    
    * refactor: Change test names
    
    * fix: Make IDs mass-assignable in pivot model
    
    * refactor: Remove association of users to publications
    
    * refactor: Remove unnecessary use statement
    
    * test: Ensure publication names are unique
    
    Co-authored-by: Sruthin <gaddamsr@msu.edu>

---
+++ backend/app/Models/Publication.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Publication extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+    ];
+
+    /**
+     * Users that belong to a publication
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'publication_users');
+    }
+}
