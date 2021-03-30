c6b37047207447d019275c26bdcf7c691bb75814
gmeben <gmeben@gmail.com>
2021-03-30 14:27:23 -0400

    feat: Add tntsearch data to scout config file

--- backend/config/scout.php
+++ backend/config/scout.php
@@ -116,4 +116,18 @@ return [
         'secret' => env('ALGOLIA_SECRET', ''),
     ],
 
+
+    'tntsearch' => [
+        'storage'  => storage_path(), //place where the index files will be stored
+        'fuzziness' => env('TNTSEARCH_FUZZINESS', false),
+        'fuzzy' => [
+            'prefix_length' => 2,
+            'max_expansions' => 50,
+            'distance' => 2
+        ],
+        'asYouType' => false,
+        'searchBoolean' => env('TNTSEARCH_BOOLEAN', false),
+        'maxDocs' => env('TNTSEARCH_MAX_DOCS', 500),
+    ],
+
 ];
