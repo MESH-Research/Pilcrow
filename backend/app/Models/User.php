8f64486ef1e415017d8d1d91009763f5d4584fe2
gmeben <gmeben@gmail.com>
2021-03-30 15:25:18 -0400

    fix: Resolve lint issue on User model

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -10,8 +10,8 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Config;
 use Laravel\Sanctum\HasApiTokens;
-use Spatie\Permission\Traits\HasRoles;
 use Laravel\Scout\Searchable;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
