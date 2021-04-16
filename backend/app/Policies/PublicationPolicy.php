4ff534bb79d9a84bf0bd6d1bafc5926256ddc684
gmeben <gmeben@gmail.com>
2021-04-16 18:59:11 -0400

    refactor: Remove unused methods

--- backend/app/Policies/PublicationPolicy.php
+++ backend/app/Policies/PublicationPolicy.php
@@ -4,36 +4,12 @@ declare(strict_types=1);
 namespace App\Policies;
 
 use App\Models\Permission;
-use App\Models\Publication;
 use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
 class PublicationPolicy
 {
     use HandlesAuthorization;
-
-    /**
-     * Determine whether the user can view any models.
-     *
-     * @param  \App\Models\User  $user
-     * @return mixed
-     */
-    public function viewAny(User $user)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can view the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Publication  $publication
-     * @return mixed
-     */
-    public function view(User $user, Publication $publication)
-    {
-        //
-    }
 
     /**
      * Determine whether the user can create models.
@@ -49,53 +25,5 @@ class PublicationPolicy
         }
 
         return false;
-    }
-
-    /**
-     * Determine whether the user can update the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Publication  $publication
-     * @return mixed
-     */
-    public function update(User $user, Publication $publication)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can delete the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Publication  $publication
-     * @return mixed
-     */
-    public function delete(User $user, Publication $publication)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can restore the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Publication  $publication
-     * @return mixed
-     */
-    public function restore(User $user, Publication $publication)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can permanently delete the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Publication  $publication
-     * @return mixed
-     */
-    public function forceDelete(User $user, Publication $publication)
-    {
-        //
     }
 }
