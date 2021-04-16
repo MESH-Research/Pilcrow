44a4b809634663bb47cc4f50c7128d9bfed0b01e
gmeben <gmeben@gmail.com>
2021-04-16 14:50:13 -0400

    test: Add tests for creating publications

--- backend/tests/Feature/PublicationTest.php
+++ backend/tests/Feature/PublicationTest.php
@@ -3,13 +3,17 @@ declare(strict_types=1);
 
 namespace Tests\Feature;
 
+use App\Models\Role;
+use App\Models\User;
 use App\Models\Publication;
 use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Nuwave\Lighthouse\Testing\MakesGraphQLRequests;
 use Tests\TestCase;
 
 class PublicationTest extends TestCase
 {
+    use MakesGraphQLRequests;
     use RefreshDatabase;
 
     public function testPublicationsCanBeCreatedWithCustomNamesThatAreNotDuplicates()
@@ -19,5 +23,42 @@ class PublicationTest extends TestCase
 
         $this->expectException(QueryException::class);
         Publication::factory()->create(['name' => 'Custom Name']);
+    }
+
+    public function testPublicationsCanBeCreatedViaMutationByAnApplicationAdministrator()
+    {
+        $user = User::factory()->create();
+        $user->assignRole(Role::APPLICATION_ADMINISTRATOR);
+        $this->actingAs($user);
+        $response = $this->graphQL(
+            'mutation CreatePublication {
+                createPublication(publication:{name:"Custom Publication for Unit Testing"}) {
+                    id
+                    name
+                }
+            }'
+        );
+        $expected_data = [
+            'createPublication' => [
+                'id' => '3',
+                'name' => 'Custom Publication for Unit Testing'
+            ]
+        ];
+        $response->assertJsonPath('data', $expected_data);
+    }
+
+    public function testPublicationsCannotBeCreatedViaMutationByARegularUser()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+        $response = $this->graphQL(
+            'mutation CreatePublication {
+                createPublication(publication:{name:"Custom Publication for Unit Testing"}) {
+                    id
+                    name
+                }
+            }'
+        );
+        $response->assertJsonPath('data', null);
     }
 }
