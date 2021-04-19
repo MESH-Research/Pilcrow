5ce1f3b7b61e8b1b7822ee9e8fc5de80248c3f43
Brian Adams <adamsb@msu.edu>
2021-04-19 10:55:19 -0400

    Change to horizontal more icon

--- client/src/components/molecules/CollapseToolbar.vue
+++ client/src/components/molecules/CollapseToolbar.vue
@@ -10,7 +10,7 @@ export default {
     },
     render: function(h) {
         const items = this.collapse ? this.$slots.default.map(vn => h('q-item', {directives: {ClosePopup}}, [h('q-item-section', {}, [vn])])) : this.$slots.default;
-        return this.collapse ? h('q-btn-dropdown', {props: {"dropdown-icon": "more_vert", flat: true, dense: true}}, [h('q-list', {}, items)]) : h('div', {}, items)
+        return this.collapse ? h('q-btn-dropdown', {props: {"dropdown-icon": "more_horiz", flat: true, dense: true}}, [h('q-list', {}, items)]) : h('div', {}, items)
     }
 }
 </script>
