5d404801e484a4133140631f3292572bba73a761
Grant Eben <gmeben@gmail.com>
2021-06-02 14:12:27 -0400

    refactor: Rename migrations file

--- backend/database/migrations/2021_06_01_195428_create_submissions_tables.php
+++ backend/database/migrations/2021_06_01_195428_create_submissions_tables.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSubmissionsTable extends Migration
+class CreateSubmissionsTables extends Migration
 {
     /**
      * Run the migrations.
