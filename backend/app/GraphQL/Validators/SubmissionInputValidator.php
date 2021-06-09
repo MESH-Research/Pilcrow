d2b0779c6adb8b894231c65a7c47a6015876a3ec
Grant Eben <gmeben@gmail.com>
2021-06-09 18:03:24 -0400

    feat: Add a validator and input type

---
+++ backend/app/GraphQL/Validators/SubmissionInputValidator.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\GraphQL\Validators;
+
+use Nuwave\Lighthouse\Validation\Validator;
+
+class SubmissionInputValidator extends Validator
+{
+    /**
+     * Return the validation rules.
+     *
+     * @return array<string, array<mixed>>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => [
+                'max:512',
+                'filled',
+            ],
+            'publication_id' => [
+                'filled',
+            ],
+            'user_id' => [
+                'filled',
+            ],
+        ];
+    }
+}
