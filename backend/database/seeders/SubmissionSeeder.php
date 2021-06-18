24f53ebe613164ab08bcf5a30d3ed8573e0fc903
Grant Eben <gmeben@gmail.com>
2021-06-18 19:59:45 -0400

    feat: Improve submission seeding

--- backend/database/seeders/SubmissionSeeder.php
+++ backend/database/seeders/SubmissionSeeder.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use App\Models\Submission;
 use Illuminate\Database\Seeder;
 
@@ -15,7 +16,13 @@ class SubmissionSeeder extends Seeder
      */
     public function run()
     {
-        Submission::factory()->create([
+        Submission::factory()->hasAttached(
+            User::where('username','regularUser')->firstOrFail(),
+            [
+                'role_id' => 6,
+            ]
+        )
+        ->create([
             'title' => 'CCR Test Submission 1',
             'publication_id' => 1,
         ]);
