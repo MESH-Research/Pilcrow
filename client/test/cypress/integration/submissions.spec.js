40bf99792bc139064cd0a30ead17365947cde9e3
Grant Eben <gmeben@gmail.com>
2021-07-02 10:02:12 -0400

    refactor: Add newlines to improve readability

--- client/test/cypress/integration/submissions.spec.js
+++ client/test/cypress/integration/submissions.spec.js
@@ -10,10 +10,15 @@ describe('Submissions', () => {
     cy.visit('submissions');
     cy.dataCy('new_submission_title_input')
       .type('Submission from Cypress{enter}');
-    cy.dataCy('new_submission_publication_input').click();
-    cy.get('.q-virtual-scroll__content').contains('Collaborative Review Organization').click();
-    cy.dataCy('save_submission').click();
-    cy.dataCy('submissions_list').contains('Submission from Cypress');
+    cy.dataCy('new_submission_publication_input')
+      .click();
+    cy.get('.q-virtual-scroll__content')
+      .contains('Collaborative Review Organization')
+      .click();
+    cy.dataCy('save_submission')
+      .click();
+    cy.dataCy('submissions_list')
+      .contains('Submission from Cypress');
     cy.injectAxe();
     cy.dataCy('create_submission_notify').should('be.visible').should('have.class','bg-positive');
     cy.checkA11y(null, {
