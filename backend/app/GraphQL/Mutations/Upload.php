34d7214a4da063b6da7ba643c21611ca730efee9
Grant Eben <gmeben@gmail.com>
2021-07-08 11:58:07 -0400

    feat: Add Laravel mutation for the upload field

---
+++ backend/app/GraphQL/Mutations/Upload.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\GraphQL\Mutations;
+
+class Upload
+{
+    /**
+     * Upload a file, store it on the server and return the path.
+     *
+     * @param  mixed  $_
+     * @param  array<string, mixed>  $args
+     * @return string|null
+     */
+    public function __invoke($_, array $args): ?string
+    {
+        /** @var \Illuminate\Http\UploadedFile $file */
+        $file = $args['file'];
+
+        return $file->storePublicly('uploads');
+    }
+}
