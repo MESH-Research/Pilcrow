2efc10e06506d16640bfe6e1966e0afd376bf64d
gaddamsr <gaddamsr@msu.edu>
2021-07-16 15:23:25 -0400

    feat(client): Filtering and autocomple for users

--- client/src/pages/SubmissionDetails.vue
+++ client/src/pages/SubmissionDetails.vue
@@ -2,18 +2,48 @@
   <q-page padding>
     <h2>{{ submission.title }}</h2>
     <p> {{ id }} </p>
+    <div id="q-app">
+      <div class="q-pa-md">
+        <div class="q-gutter-md row">
+          <q-select
+            v-model="model"
+            filled
+            use-input
+            hide-selected
+            fill-input
+            input-debounce="0"
+            hint="Minimum 2 characters to trigger filtering"
+            style="width: 250px; padding-bottom: 32px"
+            :options="options"
+            @filter="filterFn"
+            @input-value="setModel"
+          >
+            <template #no-option>
+              <q-item>
+                <q-item-section class="text-grey">
+                  No results
+                </q-item-section>
+              </q-item>
+            </template>
+          </q-select>
+        </div>
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script>
 import { GET_SUBMISSION } from "src/graphql/queries";
 
+const stringOptions = [
+  'Google', 'Facebook', 'Twitter', 'Apple', 'Oracle'
+]
 export default {
   components: {
   },
   props: {
     id: {
-      type: Number,
+      type: String,
       required: true,
     },
   },
@@ -22,8 +52,25 @@ export default {
       submission: {
         title: null,
         publication: null,
-        users: null,
+        user: null,
       },
+      model: null,
+      options: stringOptions,
+    }
+  },
+  methods: {
+    filterFn (val, update, abort) {
+      if (val.length < 2) {
+        abort()
+        return
+      }
+      update(() => {
+        const needle = val.toLowerCase()
+        this.options = stringOptions.filter(v => v.toLowerCase().indexOf(needle) > -1)
+      })
+    },
+    setModel (val) {
+      this.model = val
     }
   },
   apollo: {
