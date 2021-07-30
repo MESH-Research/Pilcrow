50d75d70438bf5cb5c2a3c996663cd5995a694c6
Grant Eben <gmeben@gmail.com>
2021-07-30 13:21:31 -0400

    test: Add Cypress test

--- client/src/pages/SubmissionDetails.vue
+++ client/src/pages/SubmissionDetails.vue
@@ -1,109 +1,114 @@
 <template>
-  <article>
+  <div
+    v-if="$apollo.loading"
+    class="q-pa-lg"
+  >
+    {{ $t('loading') }}
+  </div>
+  <article v-else>
     <h2 class="q-pl-lg">
       Manage: {{ submission.title }}
     </h2>
-    <div id="q-app">
-      <div class="row q-col-gutter-lg q-pa-lg">
-        <section class="col-md-5 col-sm-6 col-xs-12">
-          <h3>Add a Reviewer</h3>
-          <div class="q-gutter-md row">
-            <q-select
-              v-model="model"
-              filled
-              use-input
-              hide-selected
-              fill-input
-              input-debounce="0"
-              hint="Search for a user to assign."
-              style="width: 250px; padding-bottom: 32px"
-              :options="options"
-              @filter="filterFn"
-              @input-value="setModel"
-            >
-              <template #no-option>
-                <q-item>
-                  <q-item-section class="text-grey">
-                    No results
-                  </q-item-section>
-                </q-item>
-              </template>
-            </q-select>
-          </div>
-          <q-btn
-            :ripple="{ center: true }"
-            color="primary"
-            class="text-uppercase"
-            label="Add"
-            no-caps
-          />
-        </section>
-        <section
-          class="col-md-5 col-sm-6 col-xs-12"
-        >
-          <h3>Assigned Reviewers</h3>
-          <q-list
-            bordered
-            separator
+    <div class="row q-col-gutter-lg q-pa-lg">
+      <section class="col-md-5 col-sm-6 col-xs-12">
+        <h3>Add a Reviewer</h3>
+        <div class="q-gutter-md row">
+          <q-select
+            v-model="model"
+            filled
+            use-input
+            hide-selected
+            fill-input
+            input-debounce="0"
+            hint="Search for a user to assign."
+            style="width: 250px; padding-bottom: 32px"
+            :options="options"
+            @filter="filterFn"
+            @input-value="setModel"
           >
-            <div v-if="userSearch.data.length">
-              <q-item
-                v-for="user in userSearch.data"
-                :key="user.id"
-                data-cy="userListItem"
-                class="q-px-lg"
+            <template #no-option>
+              <q-item>
+                <q-item-section class="text-grey">
+                  No results
+                </q-item-section>
+              </q-item>
+            </template>
+          </q-select>
+        </div>
+        <q-btn
+          :ripple="{ center: true }"
+          color="primary"
+          class="text-uppercase"
+          label="Add"
+          no-caps
+        />
+      </section>
+      <section
+        class="col-md-5 col-sm-6 col-xs-12"
+      >
+        <h3>Assigned Reviewers</h3>
+        <q-list
+          bordered
+          separator
+          data-cy="assignedReviewersList"
+        >
+          <div v-if="userSearch.data.length">
+            <q-item
+              v-for="user in userSearch.data"
+              :key="user.id"
+              data-cy="userListItem"
+              class="q-px-lg"
+            >
+              <q-item-section
+                top
+                avatar
               >
-                <q-item-section
-                  top
-                  avatar
-                >
-                  <avatar-image
-                    :user="user"
-                    rounded
-                  />
-                </q-item-section>
+                <avatar-image
+                  :user="user"
+                  rounded
+                />
+              </q-item-section>
 
-                <q-item-section>
-                  <q-item-label v-if="user.name">
-                    {{ user.name }}
-                  </q-item-label>
-                  <q-item-label v-else>
-                    {{ user.username }}
-                  </q-item-label>
-                  <q-item-label
-                    caption
-                    lines="1"
-                  >
-                    {{ user.email }}
-                  </q-item-label>
-                </q-item-section>
-
-                <q-item-section
-                  side
-                  center
+              <q-item-section>
+                <q-item-label v-if="user.name">
+                  {{ user.name }}
+                </q-item-label>
+                <q-item-label v-else>
+                  {{ user.username }}
+                </q-item-label>
+                <q-item-label
+                  caption
+                  lines="1"
                 >
-                  <q-btn
-                    :aria-label="`Unassign ${user.username}`"
-                    flat
-                    color="primary"
-                    icon="person_remove"
-                  />
-                </q-item-section>
-              </q-item>
-            </div>
-            <div v-else>
-              <q-item class="text-grey">
-                <q-item-section avatar>
-                  <q-icon name="o_do_disturb_on" />
-                </q-item-section>
-                <q-item-section>
-                  No reviewers are assigned to this submission.
-                </q-item-section>
-              </q-item>
-            </div>
-          </q-list>
-        </section>
-      </div>
+                  {{ user.email }}
+                </q-item-label>
+              </q-item-section>
+
+              <q-item-section
+                side
+                center
+              >
+                <q-btn
+                  :aria-label="`Unassign ${user.username}`"
+                  flat
+                  color="primary"
+                  icon="person_remove"
+                />
+              </q-item-section>
+            </q-item>
+          </div>
+          <div v-else>
+            <q-item class="text-grey">
+              <q-item-section avatar>
+                <q-icon name="o_do_disturb_on" />
+              </q-item-section>
+              <q-item-section>
+                No reviewers are assigned to this submission.
+              </q-item-section>
+            </q-item>
+          </div>
+        </q-list>
+      </section>
     </div>
   </article>
 </template>
