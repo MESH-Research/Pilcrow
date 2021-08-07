b000595d7859fa4107dfda29fa6713c3661e0235
Grant Eben <gmeben@gmail.com>
2021-08-06 21:54:41 -0400

    feat: Add useful random defaults to factory

--- backend/database/factories/SubmissionUserFactory.php
+++ backend/database/factories/SubmissionUserFactory.php
@@ -3,7 +3,10 @@ declare(strict_types=1);
 
 namespace Database\Factories;
 
+use App\Models\Role;
+use App\Models\Submission;
 use App\Models\SubmissionUser;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class SubmissionUserFactory extends Factory
@@ -23,9 +26,9 @@ class SubmissionUserFactory extends Factory
     public function definition()
     {
         return [
-            'user_id' => null,
-            'submission_id' => null,
-            'role_id' => null,
+            'user_id' => User::inRandomOrder()->first()->id,
+            'submission_id' => Submission::inRandomOrder()->first()->id,
+            'role_id' => Role::inRandomOrder()->first()->id,
         ];
     }
 }
