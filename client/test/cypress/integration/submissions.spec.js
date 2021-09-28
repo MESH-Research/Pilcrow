acb310455eeb11ddea8421d39c4ca89ffaf27bf7
Grant Eben <gmeben@gmail.com>
2021-09-28 14:49:33 -0400

    style: Fix lint errors on Cypress spec for Submissions

--- client/test/cypress/integration/submissions.spec.js
+++ client/test/cypress/integration/submissions.spec.js
@@ -1,50 +1,48 @@
 /// <reference types="Cypress" />
 /// <reference path="../support/index.d.ts" />
 
-import 'cypress-axe';
-import 'cypress-file-upload';
+import "cypress-axe"
+import "cypress-file-upload"
 
-describe('Submissions', () => {
-  it('creates new submissions', () => {
-    cy.task('resetDb');
-    cy.login({ email: "applicationadministrator@ccrproject.dev" });
-    cy.visit('submissions');
-    cy.injectAxe();
-    cy.dataCy('new_submission_title_input')
-      .type('Submission from Cypress{enter}');
-    cy.dataCy('new_submission_publication_input')
-      .click();
-    cy.get('.publication_options')
-      .contains('CCR Test Publication 1')
-      .click();
-    cy.dataCy('new_submission_file_upload_input')
-      .attachFile('test.txt');
-    cy.dataCy('save_submission')
-      .click();
-    cy.dataCy('submissions_list')
-      .contains('Submission from Cypress');
-    cy.dataCy('create_submission_notify').should('be.visible').should('have.class','bg-positive');
-    cy.get('.q-notification--top-enter-active').should('not.exist');
+describe("Submissions", () => {
+  it("creates new submissions", () => {
+    cy.task("resetDb")
+    cy.login({ email: "applicationadministrator@ccrproject.dev" })
+    cy.visit("submissions")
+    cy.injectAxe()
+    cy.dataCy("new_submission_title_input").type(
+      "Submission from Cypress{enter}"
+    )
+    cy.dataCy("new_submission_publication_input").click()
+    cy.get(".publication_options").contains("CCR Test Publication 1").click()
+    cy.dataCy("new_submission_file_upload_input").attachFile("test.txt")
+    cy.dataCy("save_submission").click()
+    cy.dataCy("submissions_list").contains("Submission from Cypress")
+    cy.dataCy("create_submission_notify")
+      .should("be.visible")
+      .should("have.class", "bg-positive")
+    cy.get(".q-notification--top-enter-active").should("not.exist")
     cy.checkA11y(null, {
       rules: {
-          'nested-interactive': { enabled: false },
+        "nested-interactive": { enabled: false },
       },
-    });
-  });
+    })
+  })
 
-  it('prevents submission creation when the title exceeds the maximum length', () => {
-    const name_513_characters = '123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123';
-    cy.task('resetDb');
-    cy.login({ email: "applicationadministrator@ccrproject.dev" });
-    cy.visit('submissions');
-    cy.injectAxe();
-    cy.dataCy('new_submission_title_input')
-      .type(name_513_characters + '{enter}');
-    cy.dataCy('create_submission_notify').should('be.visible').should('have.class','bg-negative');
-    cy.get('.q-notification--top-enter-active').should('not.exist');
-    cy.checkA11y();
-  });
-
-});
-
-
+  it("prevents submission creation when the title exceeds the maximum length", () => {
+    const name_513_characters =
+      "123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123"
+    cy.task("resetDb")
+    cy.login({ email: "applicationadministrator@ccrproject.dev" })
+    cy.visit("submissions")
+    cy.injectAxe()
+    cy.dataCy("new_submission_title_input").type(
+      name_513_characters + "{enter}"
+    )
+    cy.dataCy("create_submission_notify")
+      .should("be.visible")
+      .should("have.class", "bg-negative")
+    cy.get(".q-notification--top-enter-active").should("not.exist")
+    cy.checkA11y()
+  })
+})
