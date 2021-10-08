f67fa923093a10108ef770c77f158bd8ba02df3a
Grant Eben <gmeben@gmail.com>
2021-10-08 10:34:35 -0400

    feat: Expand policy for review coordinators

--- backend/app/Policies/SubmissionUserPolicy.php
+++ backend/app/Policies/SubmissionUserPolicy.php
@@ -5,6 +5,7 @@ namespace App\Policies;
 
 use App\Models\Permission;
 use App\Models\User;
+use App\Models\SubmissionUser;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
 class SubmissionUserPolicy
@@ -14,20 +15,27 @@ class SubmissionUserPolicy
     /**
      * Determine whether the user can create the model
      *
+     * TODO: Consider implementing a more maintainable pattern than a switch or series of if/else statements
+     * TODO: Expand policy for the creation of other roles of submission users besides reviewers
+     *
      * @param  \App\Models\User  $user
      * @param  array  $model
      * @return bool
      */
     public function create(User $user, array $model)
     {
-        // TODO: is user assigned to submission and is a review coordinator
-
-        // TODO: Consider implementing a more maintainable pattern than a switch or series of if/else statements
         $is_assigning_a_reviewer = $model['role_id'] == 5; // TODO: Use a constant for this ID
         if ($is_assigning_a_reviewer) {
             return $user->can(Permission::ASSIGN_REVIEWER);
         }
 
-        return true; // TODO: Check for the other roles
+        $is_assigned_as_review_coordinator = SubmissionUser::where('user_id', $user->id,)
+            ->where('role_id', 4) // TODO: Use a constant for this ID
+            ->where('submission_id', $model['submission_id'])->first();
+        if ($is_assigned_as_review_coordinator && $is_assigning_a_reviewer) {
+            return true;
+        }
+
+        return true;
     }
 }
