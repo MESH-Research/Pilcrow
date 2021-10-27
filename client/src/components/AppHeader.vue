a30174e4f0797bdd1b5a8cf2b6a56bf884a20065
Brian Adams <adamsb@msu.edu>
2021-10-27 16:46:07 -0400

    Remove extra links in account menu

--- client/src/components/AppHeader.vue
+++ client/src/components/AppHeader.vue
@@ -40,30 +40,7 @@
                 {{ $t("header.account_link") }}
               </q-item-section>
             </q-item>
-            <q-item clickable data-cy="link_all_users" to="/admin/users">
-              <q-item-section avatar>
-                <q-icon name="groups" />
-              </q-item-section>
-              <q-item-section>
-                {{ $t("header.user_list") }}
-              </q-item-section>
-            </q-item>
-            <q-item clickable to="/admin/publications">
-              <q-item-section avatar>
-                <q-icon name="collections_bookmark" />
-              </q-item-section>
-              <q-item-section>
-                {{ $t("header.publications") }}
-              </q-item-section>
-            </q-item>
-            <q-item v-ripple to="/submissions">
-              <q-item-section avatar>
-                <q-icon name="content_copy" />
-              </q-item-section>
-              <q-item-section>
-                {{ $t("header.submissions") }}
-              </q-item-section>
-            </q-item>
+
             <q-separator />
             <q-item clickable @click="logout">
               <q-item-section avatar>
