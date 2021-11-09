c74f40b4e75ad1a4c779bfd897268da1fb179cfd
Grant Eben <gmeben@gmail.com>
2021-11-09 16:35:15 -0500

    test: Add assertions to PHPUnit test

--- backend/tests/Feature/NotificationTest.php
+++ backend/tests/Feature/NotificationTest.php
@@ -40,12 +40,111 @@ class NotificationTest extends TestCase
             'body' => 'A submission has been created.',
             'action' => 'Visit CCR',
             'url' => '/',
-            'submission_id' => 1000,
+            'submission_id' => 1001,
         ];
         Notification::send($users, new SubmissionCreation($notification_data));
         $users->map(function($user) {
             $this->assertEquals(1, $user->notifications->count());
             $this->assertEquals("App\Notifications\SubmissionCreation", $user->notifications->first()->type);
+        });
+    }
+
+    /**
+     * @return void
+     */
+    public function testMultipleSubmissionCreationNotificationsForAnIndividualUser()
+    {
+        $user = User::factory()->create();
+        $notification_data_1 = [
+            'body' => 'A submission has been created.',
+            'action' => 'Visit CCR',
+            'url' => '/',
+            'submission_id' => 1002,
+        ];
+        $notification_data_2 = [
+            'body' => 'A submission has been created.',
+            'action' => 'Visit CCR',
+            'url' => '/',
+            'submission_id' => 1003,
+        ];
+        $user->notify(new SubmissionCreation($notification_data_1));
+        $user->notify(new SubmissionCreation($notification_data_2));
+        $this->assertEquals(2, $user->notifications->count());
+        $this->assertEquals("App\Notifications\SubmissionCreation", $user->notifications->first()->type);
+        $this->assertEquals("App\Notifications\SubmissionCreation", $user->notifications->last()->type);
+    }
+
+    /**
+     * @return void
+     */
+    public function testMultipleSubmissionCreationNotificationsForMultipleUsers()
+    {
+        $users = User::factory()->count(4)->create();
+        $notification_data_1 = [
+            'body' => 'A submission has been created.',
+            'action' => 'Visit CCR',
+            'url' => '/',
+            'submission_id' => 1004,
+        ];
+        $notification_data_2 = [
+            'body' => 'A submission has been created.',
+            'action' => 'Visit CCR',
+            'url' => '/',
+            'submission_id' => 1005,
+        ];
+        Notification::send($users, new SubmissionCreation($notification_data_1));
+        Notification::send($users, new SubmissionCreation($notification_data_2));
+        $users->map(function($user) {
+            $this->assertEquals(2, $user->notifications->count());
+            $this->assertEquals("App\Notifications\SubmissionCreation", $user->notifications->first()->type);
+            $this->assertEquals("App\Notifications\SubmissionCreation", $user->notifications->last()->type);
+        });
+    }
+
+    /**
+     * @return void
+     */
+    public function testMarkingASubmissionCreationNotificationAsReadForAnIndividualUser()
+    {
+        $user = User::factory()->create();
+        $notification_data = [
+            'body' => 'A submission has been created.',
+            'action' => 'Visit CCR',
+            'url' => '/',
+            'submission_id' => 1006,
+        ];
+        $user->notify(new SubmissionCreation($notification_data));
+        $notification = $user->notifications->first();
+        $notification->markAsRead();
+        $this->assertEquals(0, $user->unreadNotifications->count());
+    }
+
+    /**
+     * @return void
+     */
+    public function testMarkingMultipleSubmissionCreationNotificationsAsReadForMultipleUsers()
+    {
+        $users = User::factory()->count(4)->create();
+        $notification_data_1 = [
+            'body' => 'A submission has been created.',
+            'action' => 'Visit CCR',
+            'url' => '/',
+            'submission_id' => 1007,
+        ];
+        $notification_data_2 = [
+            'body' => 'A submission has been created.',
+            'action' => 'Visit CCR',
+            'url' => '/',
+            'submission_id' => 1008,
+        ];
+        Notification::send($users, new SubmissionCreation($notification_data_1));
+        Notification::send($users, new SubmissionCreation($notification_data_2));
+
+        $users->map(function($user) {
+            $user->notifications->map(function($notification) {
+                $notification->markAsRead();
+            });
+            $this->assertEquals(0, $user->unreadNotifications->count());
         });
     }
 }
