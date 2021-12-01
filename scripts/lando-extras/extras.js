e59761e714401dd61363577c250ca19ddc2bf229
Amaresh Joshi <joshia@msu.edu>
2021-12-01 14:30:14 -0500

    Update extras.js
    must import be at top of file?

--- scripts/lando-extras/extras.js
+++ scripts/lando-extras/extras.js
@@ -1,5 +1,5 @@
-const { program } = require("commander");
 import chalk from 'chalk';
+const { program } = require("commander");
 const columnify = require("columnify");
 const yesno = require("yesno");
 const dedent = require("dedent-js");
