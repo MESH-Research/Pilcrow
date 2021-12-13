8bc753da633d0dff9d2618991db8590fe113e2ef
Grant Eben <gmeben@gmail.com>
2021-12-13 16:47:33 -0500

    ref: Remove unused event and listener

--- backend/app/Providers/EventServiceProvider.php
+++ backend/app/Providers/EventServiceProvider.php
@@ -18,9 +18,6 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
-        SubmissionCreated::class => [
-            SendSubmissionCreatedNotification::class,
-        ],
     ];
 
     /**
