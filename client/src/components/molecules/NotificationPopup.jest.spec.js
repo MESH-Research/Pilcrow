f8a750c55fb6e463261ff7058f395ae065684022
Grant Eben <gmeben@gmail.com>
2021-12-15 17:09:18 -0500

    test: Finalize assertions in Jest testing

--- client/src/components/molecules/NotificationPopup.jest.spec.js
+++ client/src/components/molecules/NotificationPopup.jest.spec.js
@@ -1,5 +1,8 @@
-import { mountQuasar } from "@quasar/quasar-app-extension-testing-unit-jest"
 import NotificationPopup from "./NotificationPopup.vue"
+import { mountQuasar } from "@quasar/quasar-app-extension-testing-unit-jest"
+import { CURRENT_USER_NOTIFICATIONS } from "src/graphql/queries"
+import { createMockClient } from "mock-apollo-client"
+import { DefaultApolloClient } from "@vue/apollo-composable"
 
 import * as All from "quasar"
 
@@ -11,47 +14,69 @@ const components = Object.keys(All).reduce((object, key) => {
   return object
 }, {})
 
-const notification_data = [
-  {
-    data: {
-      id: "df91c4c0-3cf2-409d-a305-9a6005149c86",
-      type: "AppNotificationsSubmissionCreated",
-      notifiable_type: "AppModelsUser",
-      notifiable_id: 1,
-      data: `"{"submission":{"id":9999,"title":"A Modest Proposal"},"publication":{"id":9999,"name":"Test Publication from Tinker"},"user":{"id":9999,"username":"Test User from Tinker"},"type":"submission.created","body":"A submission has been created.","action":"Visit CCR","url":"/"}"`,
-      read_at: null,
-      created_at: "2021-12-13 20:13:05",
-      updated_at: "2021-12-13 20:13:05",
-    },
-  },
-]
-
-jest.mock("@vue/apollo-composable", () => {
-  const { result } = notification_data
-  return {
-    useQuery: jest.fn(() => ({ result })),
-  }
-})
-
 describe("Nofitication Popup", () => {
-  const wrapper = mountQuasar(NotificationPopup, {
-    quasar: {
-      components,
-    },
-    mount: {
-      type: "full",
-      mocks: {
-        $t: (token) => token,
+  const wrapperFactory = (mocks = []) => {
+    const apolloProvider = {}
+    const mockClient = createMockClient()
+    apolloProvider[DefaultApolloClient] = mockClient
+
+    mocks?.forEach((mock) => {
+      mockClient.setRequestHandler(...mock)
+    })
+
+    return {
+      wrapper: mountQuasar(NotificationPopup, {
+        quasar: {
+          components,
+        },
+        mount: {
+          provide: apolloProvider,
+          type: "full",
+          mocks: {
+            $t: (token) => token,
+          },
+        },
+      }),
+      mockClient,
+    }
+  }
+
+  const notification_data = [
+    {
+      data: {
+        id: "df91c4c0-3cf2-409d-a305-9a6005149c86",
+        type: "AppNotificationsSubmissionCreated",
+        notifiable_type: "AppModelsUser",
+        notifiable_id: 1,
+        data: `"{"submission":{"id":9999,"title":"A Modest Proposal"},"publication":{"id":9999,"name":"Test Publication from Tinker"},"user":{"id":9999,"username":"Test User from Tinker"},"type":"submission.created","body":"A submission has been created.","action":"Visit CCR","url":"/"}"`,
+        read_at: null,
+        created_at: "2021-12-13 20:13:05",
+        updated_at: "2021-12-13 20:13:05",
       },
     },
-  })
+  ]
 
   it("mounts without errors", () => {
-    expect(wrapper).toBeTruthy()
+    const { mockClient } = wrapperFactory()
+    const queryHandler = jest.fn().mockResolvedValue(notification_data)
+    mockClient.setRequestHandler(CURRENT_USER_NOTIFICATIONS, queryHandler)
+    expect(wrapperFactory().wrapper).toBeTruthy()
   })
 
   it("displays an indicator for a user that has notifications", async () => {
-    expect(wrapper).toContain(indicator)
+    const { wrapper, mockClient } = wrapperFactory()
+    const queryHandler = jest.fn().mockResolvedValue(notification_data)
+    mockClient.setRequestHandler(CURRENT_USER_NOTIFICATIONS, queryHandler)
     const indicator = wrapper.findComponent({ ref: "notification_indicator" })
+    expect(indicator).toBeTruthy()
+  })
+
+  it("displays no indicator for a user that has no notifications", async () => {
+    const { wrapper, mockClient } = wrapperFactory()
+    const queryHandler = jest.fn().mockResolvedValue([])
+    mockClient.setRequestHandler(CURRENT_USER_NOTIFICATIONS, queryHandler)
+    expect(
+      wrapper.findAllComponents({ ref: "notification_indicator" })
+    ).toHaveLength(0)
   })
 })
