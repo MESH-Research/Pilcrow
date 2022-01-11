27159dfc54659e2051a54f94c1cba380dac0171f
Grant Eben <gmeben@gmail.com>
2022-01-11 14:19:27 -0500

    feat: Trigger notifications upon submission creation

---
+++ backend/app/Events/SubmissionCreated.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class SubmissionCreated
+{
+    use Dispatchable;
+    use InteractsWithSockets;
+    use SerializesModels;
+
+    public $submission;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct($submission)
+    {
+        $this->submission = $submission;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
+    }
+}
---
+++ backend/app/Listeners/EmailUsersAboutCreatedSubmission.php
@@ -0,0 +1,61 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Listeners;
+
+use App\Models\Role;
+use App\Notifications\SubmissionCreated;
+use Illuminate\Support\Facades\Notification;
+
+class EmailUsersAboutCreatedSubmission
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * TODO: Refactor the user data to use the "created_by" property once it's added to submissions
+     *
+     * @param  object  $event
+     * @return void
+     */
+    public function handle($event)
+    {
+        $submitters = $event->submission->users->filter(function ($user) {
+            return $user->pivot->role_id == Role::SUBMITTER_ROLE_ID;
+        });
+        $notification_data = [
+            'submission' => [
+                'id' => $event->submission->id,
+                'title' => $event->submission->title,
+            ],
+            'user' => [
+                'id' => $submitters->first()->id,
+                'username' => $submitters->first()->username,
+                'name' => $submitters->first()->name,
+            ],
+            'publication' => [
+                'id' => $event->submission->publication->id,
+                'name' => $event->submission->publication->name,
+            ],
+            'type' => 'submission.created',
+            'action' => 'Visit CCR',
+            'url' => '/',
+            'body' => 'A submission has been created.',
+        ];
+        // Notify submitters and editors
+        Notification::send($submitters, new SubmissionCreated($notification_data));
+        $editors = $event->submission->publication->users->filter(function ($user) {
+            return $user->pivot->role_id == Role::EDITOR_ROLE_ID;
+        });
+        Notification::send($editors, new SubmissionCreated($notification_data));
+    }
+}
