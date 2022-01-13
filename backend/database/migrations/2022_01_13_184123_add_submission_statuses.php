03245e713834b4859be32949966e2e7f20e49377
Grant Eben <gmeben@gmail.com>
2022-01-13 18:34:45 -0500

    build: Add migration for submission status column

---
+++ backend/database/migrations/2022_01_13_184123_add_submission_statuses.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddSubmissionStatuses extends Migration
+{
+    /** @var string $table_name */
+    private $table_name = 'submissions';
+
+    /** @var string $column_name */
+    private $column_name = 'status';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table($this->table_name, function (Blueprint $table) {
+            $table->tinyInteger($this->column_name)->default(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        if(Schema::hasColumn($this->table_name, $this->__visibility_flag)) {
+            Schema::table($this->table_name, function (Blueprint $table) {
+                $table->dropColumn($this->column_name);
+            });
+        }
+    }
+}
