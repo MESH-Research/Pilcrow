aff33f64e503efc9188fcb9823a313c66f83dba8
Grant Eben <gmeben@gmail.com>
2022-01-17 16:44:44 -0500

    feat: Add functionality for marking read multiple notifications

--- backend/app/GraphQL/Mutations/MarkNotificationReadStatus.php
+++ backend/app/GraphQL/Mutations/MarkNotificationReadStatus.php
@@ -4,18 +4,20 @@ declare(strict_types=1);
 namespace App\GraphQL\Mutations;
 
 use Illuminate\Notifications\DatabaseNotification as Notification;
+// use Illuminate\Notifications\DatabaseNotificationCollection as Collection;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Auth;
 
 class MarkNotificationReadStatus
 {
     /**
-     * Create a submission with a user and file upload
+     * Mark a single user notification as read
      *
      * @param  mixed  $_
      * @param  array<string, mixed>  $args
      * @return Notification
      */
-    public function __invoke($_, array $args): Notification
+    public function markAsRead($_, array $args): Notification
     {
         $user = Auth::user();
         $notification = Notification::where('id', $args['id'])
@@ -24,5 +26,19 @@ class MarkNotificationReadStatus
             ->firstOrFail();
         $notification->markAsRead();
         return $notification;
+    }
+
+    /**
+     * Mark all of a user's notifications as read
+     *
+     * @return int
+     */
+    public function markAllAsRead(): int
+    {
+        $user = Auth::user();
+        return $user->unreadNotifications->map(function ($notification) {
+            $notification->markAsRead();
+            return $notification;
+        })->count();
     }
 }
