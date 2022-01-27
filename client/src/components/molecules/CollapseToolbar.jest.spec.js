f5bcbfdbf0f9abce8f6bb1c5042971674ab5d4c6
Brian Adams <adamsb@msu.edu>
2022-01-27 14:10:53 -0500

    Add tests for Collapse toolbar

---
+++ client/src/components/molecules/CollapseToolbar.jest.spec.js
@@ -0,0 +1,45 @@
+import CollapseToolbar from "./CollapseToolbar.vue"
+import { mount } from "@vue/test-utils"
+import { installQuasarPlugin } from "@quasar/quasar-app-extension-testing-unit-jest"
+
+installQuasarPlugin()
+describe("CollapseToolbar", () => {
+  const makeWrapper = (slotContent, props) => {
+    return mount(CollapseToolbar, {
+      global: {
+        mocks: {
+          $t: (t) => t,
+        },
+      },
+      slots: {
+        default: slotContent,
+      },
+      props: {
+        ...props,
+      },
+    })
+  }
+  const defaultContent =
+    "<div class='btn-1'>1BTN</div><div class='btn-2'>2BTN</div>"
+
+  test("able to mount", () => {
+    const wrapper = makeWrapper(defaultContent, {})
+    expect(wrapper).toBeTruthy()
+  })
+
+  test("transparent wrapper when not collapsed", () => {
+    const wrapper = makeWrapper(defaultContent, {})
+
+    const items = wrapper.findAll("[data-v-app=''] > div")
+    expect(items).toHaveLength(2)
+    expect(items[0].classes()).toContain("btn-1")
+  })
+
+  test("dropdown is element is collapsed", async () => {
+    const wrapper = makeWrapper(defaultContent, { collapse: true })
+
+    const items = wrapper.findAll("q-item")
+    expect(items).toHaveLength(2)
+    expect(items[0].find("div.btn-1").exists()).toBeTruthy()
+  })
+})
--- client/src/components/molecules/CollapseToolbar.vue
+++ client/src/components/molecules/CollapseToolbar.vue
@@ -10,22 +10,22 @@ export default {
     },
   },
   render: function () {
-    const items = this.collapse
-      ? this.$slots
-          .default()
-          .map((vn) =>
-            h("q-item", { directives: { ClosePopup } }, [
-              h("q-item-section", {}, [vn]),
-            ])
-          )
-      : this.$slots.default()
-    return this.collapse
-      ? h(
-          "q-btn-dropdown",
-          { props: { "dropdown-icon": "more_horiz", flat: true, dense: true } },
-          [h("q-list", {}, items)]
+    if (this.$props.collapse) {
+      const items = this.$slots
+        .default()
+        .map((vn) =>
+          h("q-item", { directives: { ClosePopup } }, [
+            h("q-item-section", {}, [vn]),
+          ])
         )
-      : h("div", {}, items)
+      return h(
+        "q-btn-dropdown",
+        { props: { "dropdown-icon": "more_horiz", flat: true, dense: true } },
+        [h("q-list", {}, items)]
+      )
+    } else {
+      return this.$slots.default()
+    }
   },
 }
 </script>
