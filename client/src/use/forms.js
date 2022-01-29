d5d7a2123f0a51d17c66e04851e4ba3df750028f
Brian Adams <adamsb@msu.edu>
2022-01-29 12:17:51 -0500

    Fix dirtyguard composable and create formstate composable

--- client/src/use/forms.js
+++ client/src/use/forms.js
@@ -1,28 +1,29 @@
 import DiscardChangesDialog from "src/components/dialogs/DiscardChangesDialog.vue"
-import { onMounted, onUnmounted } from "vue"
+import { computed, onMounted, onUnmounted } from "vue"
 import { onBeforeRouteLeave } from "vue-router"
+import { useQuasar } from "quasar"
 
-export function useDirtyGuard(dirtyRef, { root }) {
+export function useDirtyGuard(dirtyRef) {
+  const { dialog } = useQuasar()
   function beforeUnload(e) {
     if (dirtyRef.value) {
       e.preventDefault()
       e.returnValue = ""
+      return "Are you sure you want to leave this page? (Changes might be lost)"
     }
   }
   function dirtyDialog() {
-    return root.$q.dialog({
+    return dialog({
       component: DiscardChangesDialog,
     })
   }
 
-  const unregisterLeave = onBeforeRouteLeave(async (_, __, next) => {
+  onBeforeRouteLeave(async (_, __, next) => {
     if (!dirtyRef.value) {
-      unregisterLeave()
       return next()
     }
-    dirtyDialog()
+    return await dirtyDialog()
       .onOk(function () {
-        unregisterLeave()
         next()
       })
       .onCancel(function () {
@@ -31,11 +32,48 @@ export function useDirtyGuard(dirtyRef, { root }) {
   })
 
   onMounted(() => {
-    window.addEventListener("beforeUnload", beforeUnload)
+    window.addEventListener("beforeunload", beforeUnload)
   })
   onUnmounted(() => {
-    window.removeEventListener("beforeUnload", beforeUnload)
+    window.removeEventListener("beforeunload", beforeUnload)
   })
 }
 
-export default { useDirtyGuard }
+export function useFormState(dirtyRef, savedRef, queries, mutations) {
+  const queryLoading = computed(() => {
+    if (Array.isArray(queries)) {
+      if (queries.find((i) => i.loading.value)) {
+        return true
+      }
+      return false
+    }
+    return queries.loading
+  })
+
+  const mutationLoading = computed(() => {
+    if (Array.isArray(mutations)) {
+      if (mutations.find((i) => i.loading.value)) {
+        return true
+      }
+      return false
+    }
+    return mutations.loading
+  })
+
+  return computed(() => {
+    if (mutationLoading.value) {
+      return "saving"
+    }
+    if (queryLoading.value) {
+      return "loading"
+    }
+    if (dirtyRef.value) {
+      return "dirty"
+    }
+    if (savedRef.value) {
+      return "saved"
+    }
+    return "idle"
+  })
+}
+export default { useDirtyGuard, useFormState }
