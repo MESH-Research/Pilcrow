ee18ebdfd17b99cbe5ed782d376e94d61a454801
Grant Eben <gmeben@gmail.com>
2022-01-31 15:50:49 -0500

    test: Improve Jest assertion

--- client/test/cypress/integration/notificationpopup.spec.js
+++ client/test/cypress/integration/notificationpopup.spec.js
@@ -5,13 +5,14 @@ import "cypress-axe"
 import "cypress-file-upload"
 
 describe("Notification Popup", () => {
-  it("allows individual notifications to be marked as read", () => {
+  it("allows an individual notification to be marked as read", () => {
     cy.task("resetDb")
     cy.login({ email: "regularuser@ccrproject.dev" })
     cy.visit("/dashboard")
     cy.dataCy("dropdown_notificiations").click()
     cy.dataCy("notification_list_item").eq(0).click()
     cy.dataCy("notification_list_item").eq(0).should("not.have.class", "unread")
+    cy.dataCy("notification_list_item").eq(1).should("have.class", "read")
   })
 
   it("allows multiple notifications to be marked as read", () => {
