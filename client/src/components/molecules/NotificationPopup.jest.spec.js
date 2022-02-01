8cd4d9975d238a27d58556b7844dce40fe07bad7
Grant Eben <gmeben@gmail.com>
2022-01-31 22:46:39 -0500

    test: Refactor Jest test spec

--- client/src/components/molecules/NotificationPopup.jest.spec.js
+++ client/src/components/molecules/NotificationPopup.jest.spec.js
@@ -37,7 +37,7 @@ describe("Nofitication Popup", () => {
           notifiable_type: "AppModelsUser",
           notifiable_id: 1,
           data: `"{"submission":{"id":9999,"title":"A Modest Proposal"},"publication":{"id":9999,"name":"Test Publication from Tinker"},"user":{"id":9999,"username":"Test User from Tinker"},"type":"submission.created","body":"A submission has been created.","action":"Visit CCR","url":"/"}"`,
-          read_at: null,
+          read_at: readStatus ? "2021-12-31 12:15:15" : null,
           created_at: "2021-12-13 20:13:05",
           updated_at: "2021-12-13 20:13:05",
         },
@@ -48,18 +48,14 @@ describe("Nofitication Popup", () => {
 
   it("mounts without errors", () => {
     const { mockClient } = wrapperFactory()
-    const queryHandler = jest
-      .fn()
-      .mockResolvedValue(getNotificationData(false))
+    const queryHandler = jest.fn().mockResolvedValue(getNotificationData(false))
     mockClient.setRequestHandler(CURRENT_USER_NOTIFICATIONS, queryHandler)
     expect(wrapperFactory().wrapper).toBeTruthy()
   })
 
   it("displays an indicator for a user that has unread notifications", async () => {
     const { wrapper, mockClient } = wrapperFactory()
-    const queryHandler = jest
-      .fn()
-      .mockResolvedValue(getNotificationData(false))
+    const queryHandler = jest.fn().mockResolvedValue(getNotificationData(false))
     mockClient.setRequestHandler(CURRENT_USER_NOTIFICATIONS, queryHandler)
     const indicator = wrapper.findComponent({ ref: "notification_indicator" })
     expect(indicator).toBeTruthy()
@@ -67,9 +63,7 @@ describe("Nofitication Popup", () => {
 
   it("displays no indicator for a user that has no unread notifications", async () => {
     const { wrapper, mockClient } = wrapperFactory()
-    const queryHandler = jest
-      .fn()
-      .mockResolvedValue(getNotificationData(true))
+    const queryHandler = jest.fn().mockResolvedValue(getNotificationData(true))
     mockClient.setRequestHandler(CURRENT_USER_NOTIFICATIONS, queryHandler)
     expect(
       wrapper.findAllComponents({ ref: "notification_indicator" })
