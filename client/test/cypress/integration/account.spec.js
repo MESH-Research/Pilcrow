205ef81e770035a5d63baadd8e4f2b3f4e61f682
Brian Adams <adamsb@msu.edu>
2022-02-01 03:41:06 -0500

    Update test to use  formactions button

--- client/test/cypress/integration/account.spec.js
+++ client/test/cypress/integration/account.spec.js
@@ -12,19 +12,19 @@ describe("Account", () => {
 
   it("can update the name field", () => {
     cy.dataCy("update_user_name").clear().type("Updated User")
-    cy.dataCy("update_user_button_save").click()
+    cy.dataCy("button_save").click()
     cy.dataCy("avatar_name").contains("Updated User")
   })
 
   it("can update the username field", () => {
     cy.dataCy("update_user_username").clear().type("updatedUser")
-    cy.dataCy("update_user_button_save").click()
+    cy.dataCy("button_save").click()
     cy.dataCy("avatar_username").contains("updatedUser")
   })
 
   it("can update the password field", () => {
     cy.dataCy("update_user_password").clear().type("XMYeygtC7TuxgER4")
-    cy.dataCy("update_user_button_save").click()
+    cy.dataCy("button_save").click()
     cy.dataCy("update_user_notify")
       .should("be.visible")
       .should("have.class", "bg-positive")
