46091f09d7aaffc9751581710cff3a2a573fa099
Sruthin86 <gaddamsr@msu.edu>
2022-03-14 14:47:51 -0400

    feat: Add tiptap editor dependencies

---
+++ client/src/components/atoms/CommentEditor.vue
@@ -0,0 +1,25 @@
+<template>
+  <editor-content :editor="editor" />
+</template>
+
+<script>
+import { useEditor, EditorContent } from '@tiptap/vue-3'
+import StarterKit from '@tiptap/starter-kit'
+
+export default {
+  components: {
+    EditorContent,
+  },
+
+  setup() {
+    const editor = useEditor({
+      content: '<p>I’m running Tiptap with Vue.js. 🎉</p>',
+      extensions: [
+        StarterKit,
+      ],
+    })
+
+    return { editor }
+  },
+}
+</script>
