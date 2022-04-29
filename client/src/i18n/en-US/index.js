cef58a45b236a277ff5c3cab08854abf4aefb30a
Brian Adams <adamsb@msu.edu>
2022-04-29 16:34:09 -0400

    fix(client,docs): Update FPR references to CCR (#1053)
    * fix(client,docs): Update FPR references to CCR
    
    * Add link to criteria popup

--- client/src/i18n/en-US/index.js
+++ client/src/i18n/en-US/index.js
@@ -487,7 +487,8 @@ export default {
   },
   dialog: {
     bypassStyleCriteria: {
-      line_1: "Style criteria are an important part of Formative Peer Review.",
+      line_1: "Style criteria are an important part of {link}.",
+      line_1_linktext: "Collaborative Community Review",
       line_2:
         "Are you sure you do not want to associate one with this comment?",
       action_submit: "Post comment as is",
