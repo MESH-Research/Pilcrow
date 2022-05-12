28796591eec5cc0af1ea60b582b971d1541d993d
Brian Adams <adamsb@msu.edu>
2022-05-12 02:22:15 -0400

    feat(api): add created_by and updated_by fields to submission
    Closes #1074

---
+++ backend/database/migrations/2022_05_12_054522_add_created_by_and_updated_by_to_submissions.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('submissions', function (Blueprint $table) {
+            $table->unsignedBigInteger('created_by');
+            $table->unsignedBigInteger('updated_by');
+
+            $table->foreign('created_by')
+                ->references('id')
+                ->on('users');
+
+            $table->foreign('updated_by')
+                ->references('id')
+                ->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('submissions', function (Blueprint $table) {
+            $table->dropForeign(['submissions_created_by_foreign', 'submissions_updated_by_foreign']);
+            $table->dropColumn(['created_by', 'updated_by']);
+        });
+    }
+};
