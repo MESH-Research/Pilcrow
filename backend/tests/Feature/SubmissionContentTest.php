c96e71b56a224a68fc7caf60d6de5f54d4284d11
Grant Eben <gmeben@gmail.com>
2022-05-20 21:12:30 -0400

    test: Add test for multiple submission content records

--- backend/tests/Feature/SubmissionContentTest.php
+++ backend/tests/Feature/SubmissionContentTest.php
@@ -32,5 +32,26 @@ class SubmissionContentTest extends TestCase
         $this->assertNotEmpty($data);
         $this->assertIsString($data);
     }
+
+    /**
+     * @return void
+     */
+    public function testAllSubmissionContentCanBeAccessedFromTheFilesOfASubmission()
+    {
+        $submission = Submission::factory()->create();
+        for ($i = 0; $i < 3; $i++) {
+            $submission_file = SubmissionFile::factory()->create([
+                'submission_id' => $submission->id
+            ]);
+            $submission_content = SubmissionContent::factory()->create([
+                'data' => 'Example content from PHPUnit ' . $i,
+                'submission_file_id' => $submission_file->id,
+            ]);
+        }
+        $submission->content_id = $submission_content->id;
+        $this->assertEquals(3, $submission->files->count());
+        $submission->files->map(function($file, $key){
+            $this->assertEquals($file->content->data, 'Example content from PHPUnit ' . $key);
+        });
     }
 }
