00e6212d28c5a9a43d85ce685bd3e878834858e1
Grant Eben <gmeben@gmail.com>
2022-05-24 10:42:44 -0400

    refactor: Clean up formatting/comments in migration

--- backend/database/migrations/2022_05_11_185450_add_submission_content.php
+++ backend/database/migrations/2022_05_11_185450_add_submission_content.php
@@ -6,12 +6,6 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-
     /** @var string $table_name */
     private $table_name = 'submissions';
 
@@ -21,17 +15,24 @@ return new class extends Migration
     /** @var string $foreign_table_name */
     private $foreign_table_name = 'submission_contents';
 
+    /**
+     * Run the migration.
+     *
+     * @return void
+     */
     public function up()
     {
-        //
         Schema::table($this->table_name, function (Blueprint $table) {
             $table->unsignedBigInteger($this->column_name)->nullable();
-            $table->foreign($this->column_name)->references('id')->on($this->foreign_table_name)->onDelete('cascade');
+            $table->foreign($this->column_name)
+                ->references('id')
+                ->on($this->foreign_table_name)
+                ->onDelete('cascade');
         });
     }
 
     /**
-     * Reverse the migrations.
+     * Reverse the migration.
      *
      * @return void
      */
