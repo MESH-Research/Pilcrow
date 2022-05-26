3bc7b17c4784e257ff4de47374c98324509cbd5e
Grant Eben <gmeben@gmail.com>
2022-05-26 13:38:11 -0400

    test: Assert primary content is correctly set on submissions

--- backend/tests/Unit/SubmissionContentTest.php
+++ backend/tests/Unit/SubmissionContentTest.php
@@ -18,19 +18,23 @@ class SubmissionContentTest extends TestCase
      */
     public function testPrimarySubmissionContentCanBeAccessedFromASubmission()
     {
-        $submission = Submission::factory()->create();
-        $submission_file = SubmissionFile::factory()->create([
-            'submission_id' => $submission->id,
-        ]);
-        $submission_content = SubmissionContent::factory()->create([
-            'data' => 'Example content from PHPUnit',
-            'submission_file_id' => $submission_file->id,
-        ]);
-        $submission->content_id = $submission_content->id;
-        $data = $submission->content->data;
-        $this->assertNotNull($data);
-        $this->assertNotEmpty($data);
-        $this->assertIsString($data);
+        $number_of_submissions = 3;
+        $submissions = Submission::factory()->count($number_of_submissions)->create();
+        $submissions->map(function($submission, $index) {
+            $submission_file = SubmissionFile::factory()->create([
+                'submission_id' => $submission->first()->id,
+            ]);
+            $submission_content = SubmissionContent::factory()->create([
+                'data' => 'Example content from PHPUnit ' . $index,
+                'submission_file_id' => $submission_file->id,
+            ]);
+            $submission->content_id = $submission_content->id;
+        });
+        $first_data = $submissions->first()->content->data;
+        $this->assertEquals($first_data, 'Example content from PHPUnit 0');
+
+        $last_data = $submissions->last()->content->data;
+        $this->assertEquals($last_data, 'Example content from PHPUnit ' . ($number_of_submissions - 1));
     }
 
     /**
