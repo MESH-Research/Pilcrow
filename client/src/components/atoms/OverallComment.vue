f7af06776b4a56fdfb430daed78ba9c4556279d8
Brian Adams <adamsb@msu.edu>
2022-06-06 13:46:32 -0400

    Nest replies inside the parent comment

--- client/src/components/atoms/OverallComment.vue
+++ client/src/components/atoms/OverallComment.vue
@@ -1,76 +1,77 @@
 <template>
-  <div ref="scrollTarget" />
-  <q-card
-    square
-    :class="{ active: isActive }"
-    class="bg-grey-1 shadow-2 q-mb-md"
-    :aria-label="
-      $t('submissions.comment.ariaLabel', {
-        username: comment.created_by.username,
-        replies: comment.replies.length,
-      })
-    "
-    data-cy="overallComment"
-  >
-    <comment-header :comment="comment" bg-color="#eeeeee" />
-    <q-card-section>
-      <!-- eslint-disable-next-line vue/no-v-html -->
-      <div v-html="comment.content" />
-    </q-card-section>
+  <div data-cy="overallComment">
+    <div ref="scrollTarget" />
+    <q-card
+      square
+      :class="{ active: isActive }"
+      class="bg-grey-1 shadow-2 q-mb-md"
+      :aria-label="
+        $t('submissions.comment.ariaLabel', {
+          username: comment.created_by.username,
+          replies: comment.replies.length,
+        })
+      "
+    >
+      <comment-header :comment="comment" bg-color="#eeeeee" />
+      <q-card-section>
+        <!-- eslint-disable-next-line vue/no-v-html -->
+        <div v-html="comment.content" />
+      </q-card-section>
 
-    <q-card-section v-if="isReplying" ref="comment_reply" class="q-pa-md">
-      <q-separator class="q-mb-md" />
-      <span class="text-h4 q-pl-sm">{{
-        $t("submissions.comment.reply.title")
-      }}</span>
-      <comment-editor :is-inline-comment="false" @cancel="cancelReply" />
-    </q-card-section>
-    <q-card-actions class="q-pa-md q-pb-lg">
-      <q-btn
-        v-if="!isReplying"
-        ref="reply_button"
-        bordered
-        color="primary"
-        label="Reply"
-        @click="initiateReply()"
-      />
-      <template v-if="hasReplies">
+      <q-card-section v-if="isReplying" ref="comment_reply" class="q-pa-md">
+        <q-separator class="q-mb-md" />
+        <span class="text-h4 q-pl-sm">{{
+          $t("submissions.comment.reply.title")
+        }}</span>
+        <comment-editor :is-inline-comment="false" @cancel="cancelReply" />
+      </q-card-section>
+      <q-card-actions class="q-pa-md q-pb-lg">
         <q-btn
-          v-if="!props.isInlineReply && !props.isOverallReply && !isCollapsed"
-          aria-label="Hide Replies"
+          v-if="!isReplying"
+          ref="reply_button"
           bordered
-          color="grey-3"
-          text-color="black"
-          @click="toggleThread"
-        >
-          <q-icon name="expand_less"></q-icon>
-          <span>Hide Replies</span>
-        </q-btn>
-        <q-btn
-          v-if="!props.isInlineReply && !props.isOverallReply && isCollapsed"
-          aria-label="Show Replies"
-          bordered
-          color="secondary"
-          text-color="white"
-          @click="toggleThread"
-        >
-          <q-icon name="expand_more"></q-icon>
-          <span>Show Replies</span>
-        </q-btn>
-      </template>
-    </q-card-actions>
-  </q-card>
-  <section class="q-mx-md">
-    <div v-if="!isCollapsed">
-      <overall-comment-reply
-        v-for="reply in comment.replies"
-        :key="reply.id"
-        ref="replyRefs"
-        :comment="reply"
-        :replies="comment.replies"
-      />
-    </div>
-  </section>
+          color="primary"
+          label="Reply"
+          @click="initiateReply()"
+        />
+        <template v-if="hasReplies">
+          <q-btn
+            v-if="!props.isInlineReply && !props.isOverallReply && !isCollapsed"
+            aria-label="Hide Replies"
+            bordered
+            color="grey-3"
+            text-color="black"
+            @click="toggleThread"
+          >
+            <q-icon name="expand_less"></q-icon>
+            <span>Hide Replies</span>
+          </q-btn>
+          <q-btn
+            v-if="!props.isInlineReply && !props.isOverallReply && isCollapsed"
+            aria-label="Show Replies"
+            bordered
+            color="secondary"
+            text-color="white"
+            @click="toggleThread"
+          >
+            <q-icon name="expand_more"></q-icon>
+            <span>Show Replies</span>
+          </q-btn>
+        </template>
+      </q-card-actions>
+    </q-card>
+    <section class="q-mx-md">
+      <div v-if="!isCollapsed">
+        <overall-comment-reply
+          v-for="reply in comment.replies"
+          :key="reply.id"
+          ref="replyRefs"
+          :comment="reply"
+          :replies="comment.replies"
+        />
+      </div>
+    </section>
+  </div>
 </template>
 <script setup>
 import { computed, inject, ref } from "vue"
