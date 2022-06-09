8ad9ca550c71d5188223bff61e2f9a2b28da508a
Grant Eben <gmeben@gmail.com>
2022-06-09 17:52:39 -0400

    fix: Update changed prop in Jest test

--- client/src/components/forms/CommentEditor.jest.spec.js
+++ client/src/components/forms/CommentEditor.jest.spec.js
@@ -77,7 +77,7 @@ describe("CommentEditor", () => {
           },
         },
         props: {
-          isInlineComment: true,
+          commentType: "overallReply",
         },
       }),
     }
