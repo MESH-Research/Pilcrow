3c1b8270b6d9e91f7dbcd03e357e9202d398672b
Brian Adams <adamsb@msu.edu>
2022-06-21 17:05:59 -0400

    Minor text changes for style criteria

--- client/src/pages/Publication/Setup/CriteriaPage.vue
+++ client/src/pages/Publication/Setup/CriteriaPage.vue
@@ -2,11 +2,17 @@
   <article class="q-px-md">
     <h2>Style Criteria</h2>
     <p>
-      Style Criteria are a core component of the Collaborative Review Process.
+      Style criteria are a core component of the Collaborative Review Process.
       Your users will associate their comments with one or more of the style
       criteria you define below. This helps keep the conversation about a
       submission aligned with your publication's core values and focused on
       moving a submission through the review process.
+    </p>
+    <p>
+      You can define up to six style critera and your reviewers are able to
+      select mulitple criteria when creating a comment. Reviewers are able to
+      bypass the style criteria selection after confirming that their comment
+      doesn't fit any of the options presented.
     </p>
     <q-banner
       v-if="publication.style_criterias.length === 0"
