81fb739f2ae38c3a5d12fb16288b0b12358e8c1a
Grant Eben <gmeben@gmail.com>
2022-06-21 15:55:31 -0400

    Save progress

--- client/src/components/atoms/SubmissionContent.jest.spec.js
+++ client/src/components/atoms/SubmissionContent.jest.spec.js
@@ -11,6 +11,29 @@ describe("SubmissionContent", () => {
       content: {
         data: "Hello world",
       },
+      inline_comments: [
+        {
+          id: "1",
+          content: "Sample Inline Comment 1",
+          created_at: "2022-06-18T03:18:08.000000Z",
+          from: 1,
+          to: 2,
+        },
+        {
+          id: "2",
+          content: "Sample Inline Comment 2",
+          created_at: "2022-06-18T03:18:08.000000Z",
+          from: 4,
+          to: 5,
+        },
+        {
+          id: "3",
+          content: "Sample Inline Comment 3",
+          created_at: "2022-06-18T03:18:08.000000Z",
+          from: 8,
+          to: 10,
+        },
+      ],
     })
     const activeComment = ref(null)
     return {
@@ -27,11 +50,21 @@ describe("SubmissionContent", () => {
           },
         },
       }),
+      props: {
+        highlightVisibility: true,
+      },
     }
   }
 
   test("able to mount", () => {
     const { wrapper } = makeTestArticle()
     expect(wrapper).toBeTruthy()
+  })
+
+  test("inline comment widgets can be toggled", async () => {
+    const { wrapper } = makeTestArticle()
+    expect(wrapper.findAll('[data-cy="comment-widget"]').length).toBe(3)
+    await wrapper.setProps({ highlightVisibility: false })
+    expect(wrapper.findAll('[data-cy="comment-widget"]').length).toBe(0)
   })
 })
