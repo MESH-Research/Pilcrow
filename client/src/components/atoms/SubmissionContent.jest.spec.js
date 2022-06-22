c016d2ec87201e359a8eb33ec4bcf75bea7066a1
Grant Eben <gmeben@gmail.com>
2022-06-22 11:05:28 -0400

    Save progress

--- client/src/components/atoms/SubmissionContent.jest.spec.js
+++ client/src/components/atoms/SubmissionContent.jest.spec.js
@@ -13,23 +13,77 @@ describe("SubmissionContent", () => {
       },
       inline_comments: [
         {
+          __typename: "InlineComment",
           id: "1",
           content: "Sample Inline Comment 1",
           created_at: "2022-06-18T03:18:08.000000Z",
+          created_by: {
+            __typename: "User",
+            email: "jestuser@ccrproject.dev",
+            id: "1",
+            name: "Jest User",
+            username: "jestUser",
+          },
+          updated_at: "2022-06-18T03:18:08.000000Z",
+          updated_by: {
+            __typename: "User",
+            email: "jestuser@ccrproject.dev",
+            id: "1",
+            name: "Jest User",
+            username: "jestUser",
+          },
+          replies: [],
+          style_criteria: [],
           from: 1,
           to: 2,
         },
         {
+          __typename: "InlineComment",
           id: "2",
           content: "Sample Inline Comment 2",
           created_at: "2022-06-18T03:18:08.000000Z",
+          created_by: {
+            __typename: "User",
+            email: "jestuser@ccrproject.dev",
+            id: "1",
+            name: "Jest User",
+            username: "jestUser",
+          },
+          updated_at: "2022-06-18T03:18:08.000000Z",
+          updated_by: {
+            __typename: "User",
+            email: "jestuser@ccrproject.dev",
+            id: "1",
+            name: "Jest User",
+            username: "jestUser",
+          },
+          replies: [],
+          style_criteria: [],
           from: 4,
           to: 5,
         },
         {
+          __typename: "InlineComment",
           id: "3",
           content: "Sample Inline Comment 3",
           created_at: "2022-06-18T03:18:08.000000Z",
+          created_by: {
+            __typename: "User",
+            email: "jestuser@ccrproject.dev",
+            id: "1",
+            name: "Jest User",
+            username: "jestUser",
+          },
+          updated_at: "2022-06-18T03:18:08.000000Z",
+          updated_by: {
+            __typename: "User",
+            email: "jestuser@ccrproject.dev",
+            id: "1",
+            name: "Jest User",
+            username: "jestUser",
+          },
+          replies: [],
+          style_criteria: [],
           from: 8,
           to: 10,
         },
@@ -63,8 +117,9 @@ describe("SubmissionContent", () => {
 
   test("inline comment widgets can be toggled", async () => {
     const { wrapper } = makeTestArticle()
-    expect(wrapper.findAll('[data-cy="comment-widget"]').length).toBe(3)
+    console.log(wrapper.vm.inlineComments)
+    expect(wrapper.findAll("[data-cy='comment-widget']").length).toBe(3)
     await wrapper.setProps({ highlightVisibility: false })
-    expect(wrapper.findAll('[data-cy="comment-widget"]').length).toBe(0)
+    expect(wrapper.findAll("[data-cy='comment-widget']").length).toBe(0)
   })
 })
