0f59543121706a72b42083e30054c6bdb0fb133c
Grant Eben <gmeben@gmail.com>
2022-06-24 13:54:25 -0400

    refactor: Adjust padding and margin on PublicationIndexPage

--- client/src/pages/Admin/PublicationIndexPage.vue
+++ client/src/pages/Admin/PublicationIndexPage.vue
@@ -1,94 +1,95 @@
 <template>
   <div>
     <h2 class="q-pl-lg">Publication Administration</h2>
-    <q-expansion-item
-      label="Create New Publication"
-      switch-toggle-side
-      header-class="bg-grey-3"
-      data-cy="create_pub_button"
-    >
-      <CreateForm @created="publicationCreated" />
-    </q-expansion-item>
-    <section v-if="!loading" class="column q-gutter-md items-center">
-      <q-list
-        v-if="publications.length !== 0"
-        bordered
-        separator
-        data-cy="publications_list"
-        class="full-width"
+    <section class="q-pa-lg">
+      <q-expansion-item
+        label="Create New Publication"
+        switch-toggle-side
+        header-class="bg-grey-3"
+        data-cy="create_pub_button"
       >
-        <q-item v-for="publication in publications" :key="publication.id">
-          <q-item-section class="q-pa-sm">
-            <q-item-label>
-              {{ publication.name }}
-            </q-item-label>
-          </q-item-section>
-          <q-item-section side>
-            <q-btn-group flat>
-              <q-btn
-                icon="visibility"
-                :to="{
-                  name: 'publication:home',
-                  params: { id: publication.id },
-                }"
-                aria-label="View Publication Page"
-              >
-                <q-tooltip :delay="500">View Publication</q-tooltip>
-              </q-btn>
+        <CreateForm @created="publicationCreated" />
+      </q-expansion-item>
+      <section v-if="!loading">
+        <q-list
+          v-if="publications.length !== 0"
+          bordered
+          separator
+          data-cy="publications_list"
+        >
+          <q-item v-for="publication in publications" :key="publication.id">
+            <q-item-section class="q-pa-sm">
+              <q-item-label>
+                {{ publication.name }}
+              </q-item-label>
+            </q-item-section>
+            <q-item-section side>
+              <q-btn-group flat>
+                <q-btn
+                  icon="visibility"
+                  :to="{
+                    name: 'publication:home',
+                    params: { id: publication.id },
+                  }"
+                  aria-label="View Publication Page"
+                >
+                  <q-tooltip :delay="500">View Publication</q-tooltip>
+                </q-btn>
 
-              <q-btn-dropdown auto-close aria-label="Configure Publication">
-                <q-list>
-                  <q-item :to="destRoute(publication.id, 'basic')">
-                    <q-item-section avatar>
-                      <q-icon name="tune" />
-                    </q-item-section>
-                    <q-item-section>
-                      {{ $t(pageTitleKey("basic")) }}
-                    </q-item-section>
-                  </q-item>
-                  <q-item :to="destRoute(publication.id, 'users')">
-                    <q-item-section avatar>
-                      <q-icon name="people" />
-                    </q-item-section>
-                    <q-item-section>
-                      {{ $t(pageTitleKey("users")) }}
-                    </q-item-section>
-                  </q-item>
-                  <q-item :to="destRoute(publication.id, 'criteria')">
-                    <q-item-section avatar>
-                      <q-icon name="card_membership" />
-                    </q-item-section>
-                    <q-item-section>
-                      {{ $t(pageTitleKey("criteria")) }}
-                    </q-item-section>
-                  </q-item>
-                  <q-item :to="destRoute(publication.id, 'content')">
-                    <q-item-section avatar>
-                      <q-icon name="toc" />
-                    </q-item-section>
-                    <q-item-section>
-                      {{ $t(pageTitleKey("content")) }}
-                    </q-item-section>
-                  </q-item>
-                </q-list>
-                <template #label>
-                  <q-icon name="settings" />
-                  <q-tooltip :delay="500">Configure Publication</q-tooltip>
-                </template>
-              </q-btn-dropdown>
-            </q-btn-group>
-          </q-item-section>
-        </q-item>
-      </q-list>
-      <q-pagination
-        v-if="paginatorInfo"
-        v-bind="binds"
-        class="col"
-        v-on="listeners"
-      />
-      <div v-else data-cy="no_publications_message">
-        No Publications Created
-      </div>
+                <q-btn-dropdown auto-close aria-label="Configure Publication">
+                  <q-list>
+                    <q-item :to="destRoute(publication.id, 'basic')">
+                      <q-item-section avatar>
+                        <q-icon name="tune" />
+                      </q-item-section>
+                      <q-item-section>
+                        {{ $t(pageTitleKey("basic")) }}
+                      </q-item-section>
+                    </q-item>
+                    <q-item :to="destRoute(publication.id, 'users')">
+                      <q-item-section avatar>
+                        <q-icon name="people" />
+                      </q-item-section>
+                      <q-item-section>
+                        {{ $t(pageTitleKey("users")) }}
+                      </q-item-section>
+                    </q-item>
+                    <q-item :to="destRoute(publication.id, 'criteria')">
+                      <q-item-section avatar>
+                        <q-icon name="card_membership" />
+                      </q-item-section>
+                      <q-item-section>
+                        {{ $t(pageTitleKey("criteria")) }}
+                      </q-item-section>
+                    </q-item>
+                    <q-item :to="destRoute(publication.id, 'content')">
+                      <q-item-section avatar>
+                        <q-icon name="toc" />
+                      </q-item-section>
+                      <q-item-section>
+                        {{ $t(pageTitleKey("content")) }}
+                      </q-item-section>
+                    </q-item>
+                  </q-list>
+                  <template #label>
+                    <q-icon name="settings" />
+                    <q-tooltip :delay="500">Configure Publication</q-tooltip>
+                  </template>
+                </q-btn-dropdown>
+              </q-btn-group>
+            </q-item-section>
+          </q-item>
+        </q-list>
+        <q-pagination
+          v-if="paginatorInfo"
+          v-bind="binds"
+          class="q-mt-md"
+          v-on="listeners"
+        />
+        <div v-else data-cy="no_publications_message">
+          No Publications Created
+        </div>
+      </section>
     </section>
   </div>
 </template>
