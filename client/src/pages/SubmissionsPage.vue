9b0cb9bed537e6d218183dcaca3b677d88f6b27a
vilagshe <sat.vilag@gmail.com>
2022-08-11 16:00:18 -0400

    style: add open/close options

--- client/src/pages/SubmissionsPage.vue
+++ client/src/pages/SubmissionsPage.vue
@@ -113,6 +113,7 @@
                     </q-item-section>
                     <q-menu anchor="bottom end" self="top end">
                       <q-item
+                        v-if="submission.status == 'INITIALLY_SUBMITTED'"
                         data-cy="accept_for_review"
                         class="items-center"
                         clickable
@@ -120,6 +121,16 @@
                           confirmHandler('accept_for_review', submission.id)
                         "
                         >{{ $t("submission.action.accept_for_review") }}</q-item
+                      >
+                      <q-item
+                        v-if="submission.status != 'INITIALLY_SUBMITTED'"
+                        data-cy="accept_as_final"
+                        class="items-center"
+                        clickable
+                        @click="
+                          confirmHandler('accept_as_final', submission.id)
+                        "
+                        >{{ $t("submission.action.accept_as_final") }}</q-item
                       >
                       <q-item
                         class="items-center"
@@ -136,6 +147,21 @@
                         clickable
                         @click="confirmHandler('reject', submission.id)"
                         >{{ $t("submission.action.reject") }}
+                      </q-item>
+                      <q-separator />
+                      <q-item
+                        v-if="submission.status == 'AWAITING_REVIEW'"
+                        class="items-center"
+                        clickable
+                        @click="confirmHandler('open', submission.id)"
+                        >{{ $t("submission.action.open") }}
+                      </q-item>
+                      <q-item
+                        v-if="submission.status == 'UNDER_REVIEW'"
+                        class="items-center"
+                        clickable
+                        @click="confirmHandler('close', submission.id)"
+                        >{{ $t("submission.action.close") }}
                       </q-item>
                     </q-menu>
                   </q-item>
