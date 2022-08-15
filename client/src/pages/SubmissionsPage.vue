ba902009c272c6ed996b03e88e6e902b083abf5f
vilagshe <sat.vilag@gmail.com>
2022-08-15 14:24:41 -0400

    feat: update context menu to show proper options

--- client/src/pages/SubmissionsPage.vue
+++ client/src/pages/SubmissionsPage.vue
@@ -112,42 +112,49 @@
                       <q-icon name="keyboard_arrow_right" />
                     </q-item-section>
                     <q-menu anchor="bottom end" self="top end">
-                      <q-item
-                        v-if="submission.status == 'INITIALLY_SUBMITTED'"
-                        data-cy="accept_for_review"
-                        class="items-center"
-                        clickable
-                        @click="
-                          confirmHandler('accept_for_review', submission.id)
-                        "
-                        >{{ $t("submission.action.accept_for_review") }}</q-item
-                      >
-                      <q-item
-                        v-if="submission.status != 'INITIALLY_SUBMITTED'"
-                        data-cy="accept_as_final"
-                        class="items-center"
-                        clickable
-                        @click="
-                          confirmHandler('accept_as_final', submission.id)
-                        "
-                        >{{ $t("submission.action.accept_as_final") }}</q-item
-                      >
-                      <q-item
-                        class="items-center"
-                        clickable
-                        @click="
-                          confirmHandler('request_resubmission', submission.id)
-                        "
-                        >{{
-                          $t("submission.action.request_resubmission")
-                        }}</q-item
-                      >
-                      <q-item
-                        class="items-center"
-                        clickable
-                        @click="confirmHandler('reject', submission.id)"
-                        >{{ $t("submission.action.reject") }}
-                      </q-item>
+                      <div v-if="submission.status != 'AWAITING_REVIEW'">
+                        <q-item
+                          v-if="submission.status == 'INITIALLY_SUBMITTED'"
+                          data-cy="accept_for_review"
+                          class="items-center"
+                          clickable
+                          @click="
+                            confirmHandler('accept_for_review', submission.id)
+                          "
+                          >{{
+                            $t("submission.action.accept_for_review")
+                          }}</q-item
+                        >
+                        <q-item
+                          v-if="submission.status != 'INITIALLY_SUBMITTED'"
+                          data-cy="accept_as_final"
+                          class="items-center"
+                          clickable
+                          @click="
+                            confirmHandler('accept_as_final', submission.id)
+                          "
+                          >{{ $t("submission.action.accept_as_final") }}</q-item
+                        >
+                        <q-item
+                          class="items-center"
+                          clickable
+                          @click="
+                            confirmHandler(
+                              'request_resubmission',
+                              submission.id
+                            )
+                          "
+                          >{{
+                            $t("submission.action.request_resubmission")
+                          }}</q-item
+                        >
+                        <q-item
+                          class="items-center"
+                          clickable
+                          @click="confirmHandler('reject', submission.id)"
+                          >{{ $t("submission.action.reject") }}
+                        </q-item>
+                      </div>
                       <q-separator />
                       <q-item
                         v-if="submission.status == 'AWAITING_REVIEW'"
