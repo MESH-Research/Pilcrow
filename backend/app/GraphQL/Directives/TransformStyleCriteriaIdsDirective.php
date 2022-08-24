44ab27a428fd71d51dc014548f641119c9a6c1ee
Grant Eben <gmeben@gmail.com>
2022-08-24 14:53:41 -0400

    feat: Add transform directive

---
+++ backend/app/GraphQL/Directives/TransformStyleCriteriaIdsDirective.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types=1);
+
+namespace App\GraphQL\Directives;
+
+use App\Models\StyleCriteria;
+use Exception;
+use Nuwave\Lighthouse\Schema\Directives\BaseDirective;
+use Nuwave\Lighthouse\Support\Contracts\ArgDirective;
+use Nuwave\Lighthouse\Support\Contracts\ArgTransformerDirective;
+
+class TransformStyleCriteriaIdsDirective extends BaseDirective implements ArgTransformerDirective, ArgDirective
+{
+    /**
+     * @return string
+     */
+    public static function definition(): string
+    {
+        //phpcs:disable
+        return /* @lang GraphQL */ <<< 'GRAPHQL'
+            """
+            Transform an array of style criteria IDs into JSON-serialized arrays
+            """
+            directive @transformStyleCriteriaIds on ARGUMENT_DEFINITION
+        GRAPHQL;
+        //phpcs:enable
+    }
+
+    /**
+     * @param string $id
+     * @return StyleCriteria
+     * @throws Exception
+     */
+    public function transform($id): StyleCriteria
+    {
+        try {
+            return StyleCriteria::where('id', $id)->firstOrFail();
+        } catch (Exception $e) {
+            throw new Exception("An invalid style criteria ID was supplied.");
+        }
+    }
+}
