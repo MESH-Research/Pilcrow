ee414f43cb28c62ca5beb7900694ec209ff6ba40
Brian Adams <adamsb@msu.edu>
2022-09-02 00:27:39 -0400

    feat(client): display version number in footer when available (#1401)

--- client/src/components/AppFooter.vue
+++ client/src/components/AppFooter.vue
@@ -6,7 +6,34 @@
         <a href="https://github.com/MESH-Research/CCR"
           >Collaborative Community Review (CCR)</a
         >
+        <span v-if="version">
+          &nbsp; Version:
+          <component :is="version_url ? 'a' : 'span'" :href="version_url">
+            {{ version }}
+            <span v-if="parsedDate && !parsedDate.invalid">
+              {{ parsedDate.toFormat("dd-LLL-yyyy T") }} ({{ version_age }})
+            </span>
+          </component>
+        </span>
       </div>
     </q-toolbar>
   </q-footer>
 </template>
+
+<script setup>
+import TimeAgo from "javascript-time-ago"
+import { DateTime } from "luxon"
+
+const timeAgo = new TimeAgo("en-US")
+
+const version = process.env.VERSION
+const version_url = process.env.VERSION_URL
+const version_date = process.env.VERSION_DATE
+
+const parsedDate = version_date ? DateTime.fromISO(version_date) : undefined
+console.log(parsedDate)
+const version_age =
+  parsedDate && !parsedDate.invalid
+    ? timeAgo.format(parsedDate.toJSDate(), "long")
+    : undefined
+</script>
--- client/src/layouts/MainLayout.vue
+++ client/src/layouts/MainLayout.vue
@@ -8,11 +8,13 @@
         <router-view />
       </q-page>
     </q-page-container>
+    <app-footer />
   </q-layout>
 </template>
 
 <script setup>
 import AppHeader from "src/components/AppHeader.vue"
+import AppFooter from "src/components/AppFooter.vue"
 import EmailVerificationBanner from "src/components/molecules/EmailVerificationBanner.vue"
 
 import { useCurrentUser } from "src/use/user"
