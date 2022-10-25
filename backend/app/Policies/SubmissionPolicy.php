7e700f4bf39961d3de70f5edf7e13696b244a40a
Sruthin86 <gaddamsr@msu.edu>
2022-10-25 17:04:27 -0400

    fix: lint errors for line length

--- backend/app/Policies/SubmissionPolicy.php
+++ backend/app/Policies/SubmissionPolicy.php
@@ -112,7 +112,10 @@ class SubmissionPolicy
             return true;
         }
 
-        if ($user->hasSubmissionRole([Role::SUBMITTER_ROLE_ID], $submission->id) && ($submission->status == Submission::DRAFT)) {
+        if (
+            $user->hasSubmissionRole([Role::SUBMITTER_ROLE_ID], $submission->id) &&
+            ($submission->status == Submission::DRAFT)
+        ) {
             return true;
         }
 
