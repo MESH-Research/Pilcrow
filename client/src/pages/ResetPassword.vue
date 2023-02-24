a20092b423ceb311a7db605974567073bcaf9559
Grant Eben <gmeben@gmail.com>
2023-02-24 12:23:31 -0500

    feat: Update Vue template to handle user interaction

--- client/src/pages/ResetPassword.vue
+++ client/src/pages/ResetPassword.vue
@@ -1,13 +1,20 @@
 <template>
-  <q-page class="flex-center flex q-pa-md" data-cy="reset_page">
+  <q-page v-if="status === 'submitted'" class="column flex-center">
+    <q-icon color="positive" name="check_circle" size="2em" />
+    <strong class="text-h3">{{ $t(`reset_password.submitted.title`) }}</strong>
+    <p>{{ $t(`reset_password.submitted.byline`) }}</p>
+  </q-page>
+  <q-page v-else class="flex-center flex q-pa-md" data-cy="reset_page">
     <q-card style="width: 400px" square>
       <q-form @submit="handleSubmit()">
         <q-card-section class="accent q-py-xs">
-          <h1 class="text-h4 text-white">Reset Password</h1>
+          <h1 class="text-h4 text-white">
+            {{ $t(`reset_password.title`) }}
+          </h1>
         </q-card-section>
-        <q-card-section class="q-pa-lg">
+        <q-card-section>
           <fieldset class="q-px-sm q-pt-md q-gutter-y-lg q-pb-lg">
-            <p>Enter your email address to request a password reset.</p>
+            <p>{{ $t(`reset_password.byline`) }}</p>
             <q-input
               ref="username"
               v-model="v$.email.$model"
@@ -27,16 +34,30 @@
             </q-input>
           </fieldset>
         </q-card-section>
+        <q-card-actions>
+          <q-btn
+            id="submitBtn"
+            ref="submitBtn"
+            type="submit"
+            :label="$t(`guiElements.form.submit`)"
+            unelevated
+            color="accent"
+            class="full-width text-white"
+            :loading="status === 'submitting'"
+          />
+        </q-card-actions>
       </q-form>
     </q-card>
   </q-page>
 </template>
 
 <script setup>
-import { reactive } from "vue"
+import { reactive, ref } from "vue"
 import { useVuelidate } from "@vuelidate/core"
 import { required, email } from "@vuelidate/validators"
-const credentials = reactive({
+import ErrorFieldRenderer from "src/components/molecules/ErrorFieldRenderer"
+const status = ref("")
+const address = reactive({
   email: "",
 })
 const rules = {
@@ -45,9 +66,15 @@ const rules = {
     required,
   },
 }
-const v$ = useVuelidate(rules, credentials)
+const v$ = useVuelidate(rules, address)
 
 const handleSubmit = function () {
-  console.log(`Hello`)
+  if (v$.value.email.$error) {
+    return
+  }
+  status.value = "submitting"
+  setTimeout(() => {
+    status.value = "submitted"
+  }, 1000)
 }
 </script>
