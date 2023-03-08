f33d6634b864e111658caf943c4ff76500977c6a
Grant Eben <gmeben@gmail.com>
2023-03-08 15:28:45 -0500

    feat: Add missing submission states with realistic update workflows

--- backend/database/seeders/SubmissionSeeder.php
+++ backend/database/seeders/SubmissionSeeder.php
@@ -21,15 +21,67 @@ class SubmissionSeeder extends Seeder
     {
         $this->callOnce(PublicationSeeder::class);
         $this->callOnce(UserSeeder::class);
+
         $this->createSubmission(100, 'Pilcrow Test Submission 1')
-            ->update(['status' => Submission::AWAITING_REVIEW, 'updated_by' => 1]);
-        $this->createSubmission(101, 'Pilcrow Test Submission 2');
+            ->update(['status' => Submission::INITIALLY_SUBMITTED, 'updated_by' => 1])
+            ->update(['status' => Submission::AWAITING_REVIEW, 'updated_by' => 1])
+            ->update(['status' => Submission::UNDER_REVIEW, 'updated_by' => 1]);
+
+        $this->createSubmission(101, 'Pilcrow Test Submission 2')
+            ->update(['status' => Submission::INITIALLY_SUBMITTED, 'updated_by' => 1]);
+
         $this->createSubmission(102, 'Pilcrow Test Submission 3')
+            ->update(['status' => Submission::INITIALLY_SUBMITTED, 'updated_by' => 1])
+            ->update(['status' => Submission::AWAITING_REVIEW, 'updated_by' => 1])
+            ->update(['status' => Submission::UNDER_REVIEW, 'updated_by' => 1])
             ->update(['status' => Submission::REJECTED, 'updated_by' => 1]);
+
         $this->createSubmission(103, 'Pilcrow Test Submission 4')
+            ->update(['status' => Submission::INITIALLY_SUBMITTED, 'updated_by' => 1])
+            ->update(['status' => Submission::AWAITING_REVIEW, 'updated_by' => 1])
+            ->update(['status' => Submission::UNDER_REVIEW, 'updated_by' => 1])
             ->update(['status' => Submission::RESUBMISSION_REQUESTED, 'updated_by' => 1]);
-        $this->createSubmission(104, 'Pilcrow Test Submission 5')
-            ->update(['status' => Submission::DRAFT, 'updated_by' => 1]);
+
+        $this->createSubmission(104, 'Pilcrow Test Submission 5');
+
+        $this->createSubmission(105, 'Pilcrow Test Submission 6')
+            ->update(['status' => Submission::INITIALLY_SUBMITTED, 'updated_by' => 1])
+            ->update(['status' => Submission::AWAITING_REVIEW, 'updated_by' => 1])
+            ->update(['status' => Submission::UNDER_REVIEW, 'updated_by' => 1])
+            ->update(['status' => Submission::ACCEPTED_AS_FINAL, 'updated_by' => 1]);
+
+        $this->createSubmission(106, 'Pilcrow Test Submission 7')
+            ->update(['status' => Submission::INITIALLY_SUBMITTED, 'updated_by' => 1])
+            ->update(['status' => Submission::AWAITING_REVIEW, 'updated_by' => 1])
+            ->update(['status' => Submission::UNDER_REVIEW, 'updated_by' => 1])
+            ->update(['status' => Submission::EXPIRED, 'updated_by' => 1]);
+
+        $this->createSubmission(107, 'Pilcrow Test Submission 8')
+            ->update(['status' => Submission::INITIALLY_SUBMITTED, 'updated_by' => 1])
+            ->update(['status' => Submission::AWAITING_REVIEW, 'updated_by' => 1])
+            ->update(['status' => Submission::UNDER_REVIEW, 'updated_by' => 1])
+            ->update(['status' => Submission::AWAITING_DECISION, 'updated_by' => 1]);
+
+        $this->createSubmission(108, 'Pilcrow Test Submission 9')
+            ->update(['status' => Submission::INITIALLY_SUBMITTED, 'updated_by' => 1])
+            ->update(['status' => Submission::AWAITING_REVIEW, 'updated_by' => 1])
+            ->update(['status' => Submission::UNDER_REVIEW, 'updated_by' => 1])
+            ->update(['status' => Submission::AWAITING_DECISION, 'updated_by' => 1])
+            ->update(['status' => Submission::AWAITING_REVIEW, 'updated_by' => 1]);
+
+        $this->createSubmission(109, 'Pilcrow Test Submission 10')
+            ->update(['status' => Submission::INITIALLY_SUBMITTED, 'updated_by' => 1])
+            ->update(['status' => Submission::AWAITING_REVIEW, 'updated_by' => 1])
+            ->update(['status' => Submission::UNDER_REVIEW, 'updated_by' => 1])
+            ->update(['status' => Submission::ACCEPTED_AS_FINAL, 'updated_by' => 1])
+            ->update(['status' => Submission::ARCHIVED, 'updated_by' => 1]);
+
+        $this->createSubmission(110, 'Pilcrow Test Submission 11')
+            ->update(['status' => Submission::INITIALLY_SUBMITTED, 'updated_by' => 1])
+            ->update(['status' => Submission::AWAITING_REVIEW, 'updated_by' => 1])
+            ->update(['status' => Submission::UNDER_REVIEW, 'updated_by' => 1])
+            ->update(['status' => Submission::REJECTED, 'updated_by' => 1])
+            ->update(['status' => Submission::DELETED, 'updated_by' => 1]);
     }
 
     /**
@@ -48,7 +100,7 @@ class SubmissionSeeder extends Seeder
             'publication_id' => 1,
             'created_by' => 1,
             'updated_by' => 1,
-            'status' => Submission::INITIALLY_SUBMITTED,
+            'status' => Submission::DRAFT,
             ...$data,
         ];
 
