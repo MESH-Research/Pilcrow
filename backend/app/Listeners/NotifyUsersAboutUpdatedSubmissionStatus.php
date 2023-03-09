158b50e7048bd5075fe4e855484fa294b05ff7bb
Grant Eben <gmeben@gmail.com>
2023-03-09 18:06:17 -0500

    fix: Add missing space to email

--- backend/app/Listeners/NotifyUsersAboutUpdatedSubmissionStatus.php
+++ backend/app/Listeners/NotifyUsersAboutUpdatedSubmissionStatus.php
@@ -188,7 +188,7 @@ class NotifyUsersAboutUpdatedSubmissionStatus
     public function underReview($default)
     {
         $default['subject'] = 'Submission Under Review';
-        $default['body'] = $default['submission']['title'] . ' has been opened for review.' .
+        $default['body'] = $default['submission']['title'] . ' has been opened for review. ' .
                             'It is currently awaiting review from the assigned reviewers.';
         $default['type'] = 'submission.under_review';
 
