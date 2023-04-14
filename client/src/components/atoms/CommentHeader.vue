f9b34886de3b401cc01e3da23969b950bc3f1efb
Grant Eben <gmeben@gmail.com>
2023-04-14 15:15:27 -0400

    feat: Make comment headers like comment preview headers
    * Use display_label instead of username
    * Add conditional ellipsis for names
    * Prevent text wrapping

--- client/src/components/atoms/CommentHeader.vue
+++ client/src/components/atoms/CommentHeader.vue
@@ -1,47 +1,52 @@
 <template>
   <q-card-section class="q-py-xs" :style="style">
-    <div class="row no-wrap justify-between">
-      <div class="row items-center">
-        <avatar-image :user="comment.created_by" round size="30px" />
-        <div class="text-h4 q-pl-sm">{{ comment.created_by.username }}</div>
+    <div class="row items-center">
+      <avatar-image
+        :user="comment.created_by"
+        round
+        size="30px"
+        class="q-mr-sm"
+      />
+      <div class="row items-center q-pr-sm" style="flex: 1; min-width: 0">
+        <div class="text-h4 ellipsis" :title="comment.created_by.display_label">
+          {{ comment.created_by.display_label }}
+        </div>
       </div>
-      <div class="row items-center">
-        <div
-          v-if="comment.updated_at != comment.created_at"
-          data-cy="timestampUpdated"
-          class="text-caption"
-          :aria-label="
-            $t('submissions.comment.dateLabelUpdated', {
-              date: relativeUpdatedTime,
-            })
-          "
-        >
-          <q-tooltip
-            >{{ createdDate.toFormat("LLL dd yyyy hh:mm a") }} <br />
-            {{ $t("submissions.comment.updatedLabel") }}
-            {{ updatedDate.toFormat("LLL dd yyyy hh:mm a") }}
-          </q-tooltip>
+      <div
+        v-if="comment.updated_at != comment.created_at"
+        data-cy="timestampUpdated"
+        class="text-caption"
+        :aria-label="
+          $t('submissions.comment.dateLabelUpdated', {
+            date: relativeUpdatedTime,
+          })
+        "
+      >
+        <q-tooltip
+          >{{ createdDate.toFormat("LLL dd yyyy hh:mm a") }} <br />
           {{ $t("submissions.comment.updatedLabel") }}
-          {{ relativeUpdatedTime }}
-        </div>
-        <div
-          v-else
-          data-cy="timestampCreated"
-          class="text-caption"
-          :aria-label="
-            $t('submissions.comment.dateLabel', { date: relativeCreatedTime })
-          "
-        >
-          <q-tooltip>
-            {{ createdDate.toFormat("LLL dd yyyy hh:mm a") }}
-          </q-tooltip>
-          {{ relativeCreatedTime }}
-        </div>
-        <comment-actions
-          @quote-reply-to="$emit('quoteReplyTo')"
-          @modify-comment="$emit('modifyComment')"
-        />
+          {{ updatedDate.toFormat("LLL dd yyyy hh:mm a") }}
+        </q-tooltip>
+        {{ $t("submissions.comment.updatedLabel") }}
+        {{ relativeUpdatedTime }}
       </div>
+      <div
+        v-else
+        data-cy="timestampCreated"
+        class="text-caption text-no-wrap"
+        :aria-label="
+          $t('submissions.comment.dateLabel', { date: relativeCreatedTime })
+        "
+      >
+        <q-tooltip>
+          {{ createdDate.toFormat("LLL dd yyyy hh:mm a") }}
+        </q-tooltip>
+        {{ relativeCreatedTime }}
+      </div>
+      <comment-actions
+        @quote-reply-to="$emit('quoteReplyTo')"
+        @modify-comment="$emit('modifyComment')"
+      />
     </div>
   </q-card-section>
 </template>
--- client/src/components/atoms/CommentPreviewHeader.vue
+++ client/src/components/atoms/CommentPreviewHeader.vue
@@ -2,11 +2,11 @@
   <q-card-section class="q-py-xs" :style="style">
     <div class="row items-center">
       <avatar-image
-          :user="comment.created_by"
-          round
-          size="30px"
-          class="q-mr-sm"
-        />
+        :user="comment.created_by"
+        round
+        size="30px"
+        class="q-mr-sm"
+      />
       <div class="row items-center q-pr-sm" style="flex: 1; min-width: 0">
         <div class="text-h4 ellipsis" :title="comment.created_by.display_label">
           {{ comment.created_by.display_label }}
