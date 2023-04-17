3fc84a8bb62780740545d14b716035644b7fb5fb
vilagshe <sat.vilag@gmail.com>
2023-04-17 14:13:32 -0400

    feat: add new content to home page

--- client/src/pages/Index.vue
+++ client/src/pages/Index.vue
@@ -1,8 +1,51 @@
 <template>
-  <q-page class="flex column flex-center q-pb-lg" data-cy="vueIndex">
-    <h1>Home Page</h1>
-    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg" />
-  </q-page>
+  <q-layout>
+    <q-page-container>
+      <q-page>
+        <q-img
+          :alt="$t('header.logo_alt')"
+          src="/logo.png"
+          style="width: 500px; aspect-ratio: 1;"
+        ></q-img>
+        <h1>
+          Cultivating openness, collaboration, and communtiy in peer review.
+        </h1>
+        <p>
+          Pilcrow is an application that facilitates the peer review process for
+          scholarly communications. It is founded on creating supportive
+          experiences for authors, reviewers, and editors and provides an
+          environment for a peer review practice with transparency,
+          collegiality, and ongoing developmemtal conversations.
+        </p>
+        <p>On Pilcrow, editorial teams can:</p>
+        <q-list tag="ul">
+          <q-item tag="li"
+            >Move a piece of scholarship from submission through peer
+            review,</q-item
+          >
+          <q-item tag="li">Export submissions for publication.</q-item>
+          <q-item tag="li"
+            >Invite, manage, and correspond with authors and reviewers.</q-item
+          >
+        </q-list>
+        <p>
+          Pilcrow is a collaboration between
+          <a href="https://www.meshresearch.net">Mesh Research</a> and the
+          <a href="https://publicphilosophyjournal.org"
+            >Public Philosophy Journal</a
+          >
+          at Michigan State University. The project is supported by the Andrew
+          W. Mellon Foundation (Grant #G-1910-07239).
+        </p>
+        <p>
+          Pilcrow is an open source project. Documentation can be found at
+          <a href="https://ccr.meshresearch.dev/"
+            >https://ccr.meshresearch.dev/</a
+          >
+        </p>
+      </q-page>
+    </q-page-container>
+  </q-layout>
 </template>
 
 <script>
