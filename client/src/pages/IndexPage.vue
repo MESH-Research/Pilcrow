51411aefb6d42927af08d99fc224d5b7c6caab6b
vilagshe <sat.vilag@gmail.com>
2023-04-21 16:00:45 -0400

    style: update link color on home page

--- client/src/pages/IndexPage.vue
+++ client/src/pages/IndexPage.vue
@@ -7,7 +7,7 @@
             :alt="$t('header.logo_alt')"
             src="/logo.png"
             style="width: 400px; aspect-ratio: 1"
-            class="q-ma-md col-xs-12 col-sm-6 col-grow "
+            class="q-ma-md col-xs-12 col-sm-6 col-grow"
           ></q-img>
           <div class="q-ma-md col-xs-8 col-sm-10 col-md-6">
             <h1>{{ $t(`home.h1`) }}</h1>
@@ -26,20 +26,22 @@
             </q-list>
             <i18n-t keypath="home.grant_statement.statement" tag="p">
               <template #mesh_url>
-                <a href="https://www.meshresearch.net">{{
+                <a href="https://www.meshresearch.net" class="hy-link">{{
                   $t(`home.grant_statement.mesh_link`)
                 }}</a>
               </template>
               <template #ppj_url>
-                <a href="https://publicphilosophyjournal.org">{{
-                  $t(`home.grant_statement.ppj_link`)
-                }}</a>
+                <a
+                  href="https://publicphilosophyjournal.org"
+                  class="hy-link"
+                  >{{ $t(`home.grant_statement.ppj_link`) }}</a
+                >
               </template>
             </i18n-t>
 
             <i18n-t keypath="home.open_source.statement" tag="p">
               <template #pilcrow_docs>
-                <a href="https://ccr.meshresearch.dev"
+                <a href="https://ccr.meshresearch.dev" class="hy-link"
                   >https://ccr.meshresearch.dev</a
                 >
               </template>
