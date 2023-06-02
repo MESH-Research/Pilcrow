a7aa44a50515d653ff1a37d508e602a3ffff82d8
Grant Eben <gmeben@gmail.com>
2023-06-02 18:24:50 -0400

    feat: Switch to coordinate and to reviews tables

--- client/src/pages/ReviewsPage.vue
+++ client/src/pages/ReviewsPage.vue
@@ -2,20 +2,22 @@
   <article>
     <h2 class="q-pl-lg">{{ $t(`reviews.page_title`) }}</h2>
     <div class="row q-col-gutter-lg q-pa-lg">
+      <section
+        v-if="coordinator_reviews.length > 0"
+        class="col-md-10 col-sm-11 col-xs-12 q-mb-md"
+      >
+        <submission-table
+          data-cy="coordinator_table"
+          :table-data="coordinator_reviews"
+          table-type="reviews"
+          role="coordinator"
+        />
+      </section>
       <section class="col-md-10 col-sm-11 col-xs-12">
         <submission-table
           data-cy="reviewer_table"
           :table-data="reviewer_reviews"
           table-type="reviews"
-        />
-      </section>
-      <section class="col-md-10 col-sm-11 col-xs-12 q-mt-md">
-        <submission-table
-          v-if="coordinator_reviews.length > 0"
-          data-cy="coordinator_table"
-          :table-data="coordinator_reviews"
-          table-type="reviews"
-          role="coordinator"
         />
       </section>
     </div>
