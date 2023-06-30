8a71a0a5e95a63ff35499ed675b8db691f97e20d
vilagshe <sat.vilag@gmail.com>
2023-06-30 14:00:49 -0400

    update: fix parenth error

--- client/src/components/forms/Publication/UpdateContentForm.vue
+++ client/src/components/forms/Publication/UpdateContentForm.vue
@@ -5,7 +5,7 @@
       data-cy="content_block_select"
       filled
       :options="options"
-      :label=$t(`publication.basic.choose_block`)
+      :label="$t(`publication.basic.choose_block`)"
     >
       <template #option="scope">
         <q-item v-bind="scope.itemProps">
