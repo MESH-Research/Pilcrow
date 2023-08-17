df932cbfb928a5fa9f2e48d7fb605325b5e1e044
Grant Eben <gmeben@users.noreply.github.com>
2023-08-17 09:16:27 -0400

    feat: Improve logging out message (#1930)
    This improves the positioning of the log out message and makes it internationalized. 
    
    Closes #1904

--- client/src/pages/LogoutPage.vue
+++ client/src/pages/LogoutPage.vue
@@ -1,5 +1,14 @@
 <template>
-  <p>Logging out...</p>
+  <q-page class="flex-center flex q-pa-md">
+    <div style="max-width: 400px">
+      <q-banner>
+        <template #avatar>
+          <q-spinner color="primary" />
+        </template>
+        <p>{{ $t("auth.logging_out") }}</p>
+      </q-banner>
+    </div>
+  </q-page>
 </template>
 
 <script setup>
