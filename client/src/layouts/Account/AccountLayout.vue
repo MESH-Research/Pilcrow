a388212233c2d1c515db743501e19dfe03af9817
Grant Eben <gmeben@gmail.com>
2023-08-23 11:17:52 -0400

    feat: Remove "Preview Public Profile and ?? placeholders

--- client/src/layouts/Account/AccountLayout.vue
+++ client/src/layouts/Account/AccountLayout.vue
@@ -11,38 +11,6 @@
           <q-card-section class="col-sm-12 col-xs-12 q-mt-md q-pa-none">
             <collapse-menu :items="items" />
           </q-card-section>
-          <q-card-section class="col-sm-12 col-xs-12 q-mt-md q-pa-none">
-            <q-list role="list">
-              <q-item>
-                <q-item-section>
-                  {{ $t(`account.submissions_created`) }}
-                </q-item-section>
-                <q-item-section
-                  avatar
-                  class="text-primary text-bold user-stats"
-                >
-                  ??
-                </q-item-section>
-              </q-item>
-              <q-item>
-                <q-item-section>
-                  {{ $t(`account.submissions_reviewed`) }}
-                </q-item-section>
-                <q-item-section
-                  avatar
-                  class="text-primary text-bold user-stats"
-                >
-                  ??
-                </q-item-section>
-              </q-item>
-              <q-item clickable class="light-grey">
-                <q-item-section avatar><q-icon name="launch" /></q-item-section>
-                <q-item-section>
-                  {{ $t(`account.preview_link`) }}
-                </q-item-section>
-              </q-item>
-            </q-list>
-          </q-card-section>
         </div>
       </q-card>
       <q-card class="col-sm-9 col-xs-12 no-shadow outline no-border-radius">
