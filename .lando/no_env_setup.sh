7af263c4e418f4db6b2d58c347c8e1a0576680ae
Brian Adams <adamsb@msu.edu>
2023-10-11 14:49:11 -0400

    fix: move .env file to root

--- .lando/no_env_setup.sh
+++ .lando/no_env_setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if [ -f "/app/backend/.env" ]; then
+if [ -f "/app/.env" ]; then
     echo "Backend .env exists, skipping initial setup"
     exit
 fi
 
 echo "No backend .env file found. Copying defaults."
 
-cp /app/.lando/backend.default.env /app/backend/.env
+cp /app/.lando/default.env /app/.env
 
 
 
