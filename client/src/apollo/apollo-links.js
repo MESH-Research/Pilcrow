faee9da477ea6dc8b9f9b49c4ba79ae56472984e
Brian Adams <adamsb@msu.edu>
2023-11-01 16:15:14 -0400

    Update client/src/apollo/apollo-links.js
    Co-authored-by: Grant Eben <gmeben@users.noreply.github.com>

--- client/src/apollo/apollo-links.js
+++ client/src/apollo/apollo-links.js
@@ -9,7 +9,7 @@ const fetchXsrfToken = async () => {
   return fetch("/sanctum/csrf-cookie", {
     credentials: "same-origin",
   }).then(async (response) => {
-    //Read response text (even though its empty) to prevent Chrome from thinking there's an error b/c no one read the (empty) response body.
+    //Read response text (even though its empty) to prevent the browser from thinking there's an error b/c no one read the (empty) response body.
     await response.text()
     const xsrfToken = cookieXsrfToken()
     return xsrfToken
