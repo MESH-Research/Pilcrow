e888ddb88a973609b8f3b104c2025b71e61cb6a3
Grant Eben <gmeben@gmail.com>
2023-11-21 15:37:16 -0500

    fix: Dashboard button

--- client/src/pages/VerifyEmail.vue
+++ client/src/pages/VerifyEmail.vue
@@ -18,9 +18,7 @@
           icon="arrow_forward"
           size="md"
           :label="$t('buttons.dashboard')"
-          :to="{
-            name: 'dashboard',
-          }"
+          to="/dashboard"
         />
       </div>
       <div v-else-if="status == 'failure'" class="column flex-center">
