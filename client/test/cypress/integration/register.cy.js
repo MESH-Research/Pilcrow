8dc386e965eadb9b9ed673ddf177ecd78e416c09
vilagshe <sat.vilag@gmail.com>
2024-01-25 13:43:19 -0500

    test: add a11y checks to register page tests

--- client/test/cypress/integration/register.cy.js
+++ client/test/cypress/integration/register.cy.js
@@ -10,6 +10,7 @@ describe("Register", () => {
 
   it("validates fields", () => {
     cy.task("resetDb")
+    cy.injectAxe()
     cy.get(".q-form").within(() => {
       //Email is required
       cy.dataCy("email_field")
@@ -17,6 +18,7 @@ describe("Register", () => {
       cy.dataCy("email_field")
         .parents("label")
         .should("have.class", "q-field--error")
+      cy.checkA11y(null, null, a11yLogViolations)
 
       //Email must be valid
       cy.dataCy("email_field")
@@ -27,6 +29,7 @@ describe("Register", () => {
         .parents("label")
         .should("have.class", "q-field--error")
         .contains("a valid email")
+      cy.checkA11y(null, null, a11yLogViolations)
 
       //Email success
       cy.dataCy("email_field")
@@ -34,12 +37,14 @@ describe("Register", () => {
       cy.dataCy("email_field")
         .parents("label")
         .should("not.have.class", "q-field--error")
+      cy.checkA11y(null, null, a11yLogViolations)
 
       //Username is required
       cy.dataCy("username_field")
         .parents("label")
         .should("have.class", "q-field--error")
         .contains("is required")
+      cy.checkA11y(null, null, a11yLogViolations)
 
       //Username success
       cy.dataCy("username_field")
@@ -47,12 +52,14 @@ describe("Register", () => {
       cy.dataCy("username_field")
         .parents("label")
         .should("not.have.class", "q-field--error")
+      cy.checkA11y(null, null, a11yLogViolations)
 
       //Password is required
       cy.dataCy("password_field")
         .parents("label")
         .should("have.class", "q-field--error")
         .contains("is required")
+      cy.checkA11y(null, null, a11yLogViolations)
 
       //Password must be complex
       cy.dataCy("password_field")
@@ -61,6 +68,7 @@ describe("Register", () => {
         .parents("label")
         .should("have.class", "q-field--error")
         .contains("be more complex")
+      cy.checkA11y(null, null, a11yLogViolations)
 
       //Password success
       cy.dataCy("password_field")
@@ -68,6 +76,7 @@ describe("Register", () => {
       cy.dataCy("password_field")
         .parents("label")
         .should("not.have.class", "q-field--error")
+      cy.checkA11y(null, null, a11yLogViolations)
 
       //Username must be unique
       cy.dataCy("username_field")
@@ -79,6 +88,7 @@ describe("Register", () => {
         .should("have.class", "q-field--error")
         .contains("is not available")
       cy.dataCy("username_field").type("brandnewusername")
+      cy.checkA11y(null, null, a11yLogViolations)
 
       //Email must be unique
       cy.dataCy("email_field")
@@ -89,6 +99,7 @@ describe("Register", () => {
         .parents("label")
         .should("have.class", "q-field--error")
         .contains("already registered")
+      cy.checkA11y(null, null, a11yLogViolations)
 
       cy.dataCy("email_field")
         .clear()
@@ -97,6 +108,7 @@ describe("Register", () => {
 
       cy.get('[type="submit"]').click()
       cy.url().should("include", "/dashboard")
+      cy.checkA11y(null, null, a11yLogViolations)
     })
   })
 
