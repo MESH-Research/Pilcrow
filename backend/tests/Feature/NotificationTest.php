55f54dd12a0e75a0308a5cd92a64d25cd3b9480b
Grant Eben <gmeben@gmail.com>
2024-08-30 22:32:58 -0400

    test: Save progress

--- backend/tests/Feature/NotificationTest.php
+++ backend/tests/Feature/NotificationTest.php
@@ -236,4 +236,59 @@ class NotificationTest extends TestCase
         $this->assertEquals(1, $reviewer->notifications->count());
         $this->assertEquals(1, $review_coordinator->notifications->count());
     }
+
+    /**
+     * @return void
+     */
+    public function testUsersReceiveNotificationsForInlineComments()
+    {
+        $submission = $this->createSubmissionWithInlineComment(4);
+        $comment = $submission->inlineComments->first();
+        $comment2 = $submission->inlineComments->splice(2, 1)->first();
+        $reviewer1 = User::factory()->create();
+        $reviewer2 = User::factory()->create();
+        $reviewer3 = User::factory()->create();
+        $submission->reviewers()->attach([$reviewer1->id, $reviewer2->id, $reviewer3->id]);
+        InlineComment::factory()->create([
+            'submission_id' => $submission->id,
+            'content' => 'This is some content for an inline comment created by PHPUnit.',
+            'created_by' => $reviewer1->id,
+            'updated_by' => $reviewer1->id,
+            'style_criteria' => [],
+            'parent_id' => $comment->id,
+            'reply_to_id' => $comment->id,
+        ]);
+        InlineComment::factory()->create([
+            'submission_id' => $submission->id,
+            'content' => 'This is some content for an inline comment created by PHPUnit.',
+            'created_by' => $reviewer2->id,
+            'updated_by' => $reviewer2->id,
+            'style_criteria' => [],
+            'parent_id' => $comment2->id,
+            'reply_to_id' => $comment2->id,
+        ]);
+        $c6 = InlineComment::factory()->create([
+            'submission_id' => $submission->id,
+            'content' => 'This is some content for an inline comment created by PHPUnit.',
+            'created_by' => $reviewer3->id,
+            'updated_by' => $reviewer3->id,
+            'style_criteria' => [],
+            'parent_id' => $comment2->id,
+            'reply_to_id' => $comment2->id,
+        ]);
+        InlineComment::factory()->create([
+            'submission_id' => $submission->id,
+            'content' => 'This is some content for an inline comment created by PHPUnit.',
+            'created_by' => $reviewer2->id,
+            'updated_by' => $reviewer2->id,
+            'style_criteria' => [],
+            'parent_id' => $comment2->id,
+            'reply_to_id' => $c6->id,
+        ]);
+
+        $comments = $submission->inlineCommentsWithReplies;
+        $data = $comments->splice(2, 1)->first()->commentors->unique()->toArray();
+
+        print_r($data);
+    }
 }
