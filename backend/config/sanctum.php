d9e13134a7be35412ece01918d62750256769e96
Brian Adams <adamsb@msu.edu>
2025-05-31 18:58:06 -0400

    fix: parse sanctum domains from APP_URL

--- backend/config/sanctum.php
+++ backend/config/sanctum.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', env('APP_HOSTNAME', 'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1'))),
+    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', parse_url(env('APP_URL', null), PHP_URL_HOST) ?? 'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1')),
 
     /*
     |--------------------------------------------------------------------------
