77718c5c81f6a57feca950c80929274881737167
Brian Adams <adamsb@msu.edu>
2025-06-14 16:27:22 -0400

    wip: ci

--- .github/actions/pilcrow-toolkit/entrypoint.sh
+++ .github/actions/pilcrow-toolkit/entrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-CMD="$1"
+CMD="commands/$1"
 
 set -e
 
-exec ${@}
+if [ ! -x "$CMD" ]; then
+    echo "Unknown command: ${1}"
+    echo "Available commands: $(ls commands/)"
+    exit 1
+fi
+
+shift
+exec $CMD ${@}
