a3b01d95a0528c1dbcb833c6d8512b9a8357679f
Brian Adams <adamsb@msu.edu>
2025-06-24 12:12:42 -0400

    wip

--- .github/actions/toolkit/src/lib/action.ts
+++
@@ -1,7 +0,0 @@
-import { ActionCommand, ActionDefinition, ActionStage } from "/src/types";
-
-export function runCommand(definition: ActionDefinition): ActionCommand {
-    return function (stage: ActionStage): Promise<void> {
-        return !!definition[stage] ? definition[stage]() : Promise.resolve();
-    };
-}
