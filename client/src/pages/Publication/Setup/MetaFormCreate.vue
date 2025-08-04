9c1945bf937f3ffc2928efc4e946ca840cd97303
Grant Eben <gmeben@gmail.com>
2025-08-04 15:06:38 -0400

    Reapply "Save progress"
    This reverts commit d38f046699367d105fb770e47f8c39d229618e12.

--- client/src/pages/Publication/Setup/MetaFormCreate.vue
+++ client/src/pages/Publication/Setup/MetaFormCreate.vue
@@ -1,39 +1,61 @@
 <template>
-  <article class="q-px-md">
+  <article v-if="loading">Loading...</article>
+  <article v-else class="q-pl-lg">
     <h2>Add Meta Form</h2>
-    <q-field borderless>
-      <q-input
-        v-model="form_name"
-        outlined
-        label="Form Name"
-        data-cy="new_meta_form_name_input"
+    <q-form data-cy="create_meta_form" @submit="handleSubmit()">
+      <q-field borderless>
+        <q-input
+          v-model="v$.form_name.$model"
+          :error="v$.form_name.$error"
+          outlined
+          label="Form Name"
+          data-cy="new_meta_form_name_input"
+        />
+      </q-field>
+      <q-field borderless class="q-my-md">
+        <q-input
+          v-model="v$.formCaption.$model"
+          :error="v$.formCaption.$error"
+          outlined
+          label="Caption"
+          data-cy="new_meta_form_caption_input"
+        />
+      </q-field>
+      <q-field borderless>
+        <q-checkbox v-model="v$.isRequired.$model" label="Required" />
+      </q-field>
+      <q-btn
+        class="accent text-white q-mt-lg"
+        type="submit"
+        label="Create"
+        :disable="saving"
+        :loading="saving"
       />
-    </q-field>
-    <q-field borderless class="q-my-md">
-      <q-input
-        v-model="form_caption"
-        outlined
-        label="Caption"
-        data-cy="new_meta_form_caption_input"
-      />
-    </q-field>
-    <q-field borderless>
-      <q-checkbox v-model="form_required" label="Required" />
-    </q-field>
-    <q-btn class="accent text-white q-mt-lg" type="submit" label="Create" />
 
-    <q-btn
-      :to="{ name: 'publication:setup:metaForms' }"
-      class="text-white q-mt-lg"
-      type="button"
-      label="Cancel"
-    ></q-btn>
+      <q-btn
+        :to="{ name: 'publication:setup:metaForms' }"
+        class="text-white q-mt-lg"
+        type="button"
+        label="Cancel"
+      ></q-btn>
+    </q-form>
   </article>
 </template>
 
 <script setup>
-import { ref } from "vue"
-const form_name = ref("")
-const form_caption = ref("")
-const form_required = ref(false)
+import { useMetaFormCreation } from "src/use/publicationMetaForm"
+
+const { createMetaForm, v$, saving } = useMetaFormCreation()
+async function handleSubmit() {
+  try {
+    await createMetaForm()
+    // const mutationResult = await createMetaForm()
+    push({
+      name: "publication:setup:metaForms"
+    })
+  } catch (e) {
+    console.error(e)
+    newStatusMessage("failure", `Something went wrong: ${e}`)
+  }
+}
 </script>
