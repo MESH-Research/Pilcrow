78d0c7fa06f814748a404a778fd2a8bd902d0478
Grant Eben <gmeben@gmail.com>
2026-03-04 17:18:47 -0500

    style: Reorder styles

--- client/src/components/styles/exportStyles.js
+++ client/src/components/styles/exportStyles.js
@@ -5,27 +5,11 @@ export default `
 .q-img {
   display: none;
 }
-.inline-comment > .comment > .comment-header > div > .q-btn[aria-label="Go To Highlight"] {
-  cursor: pointer;
-  display: inline-block;
-}
-.inline-comment > .comment > .comment-header > div > .q-btn[aria-label="Go To Highlight"] * {
-  pointer-events: none;
-}
-.inline-comment > .comment > .comment-header > div > .q-btn[aria-label="Go To Highlight"] i {
-  font-size: 0 !important;
-}
-.inline-comment > .comment > .comment-header > div > .q-btn[aria-label="Go To Highlight"] i:before {
-  content: "⇧";
-  display: inline-block;
-  font-size: 1rem;
-  font-style: normal;
-}
 .q-chip {
   background: #000;
+  border-radius: 16px;
   color: #fff;
   display: inline-block;
-  border-radius: 16px;
   padding: 4px 16px;
 }
 .text-h3 {
@@ -34,6 +18,21 @@ export default `
 }
 .review-controls {
   display: none;
+}
+.comment-header .text-h4 {
+  font-weight: bold;
+}
+aside.q-drawer {
+  transform: none !important;
+  width: auto !important;
+}
+.comment-highlight {
+  background-color: #c9e5f8;
+}
+.style-criteria-section {
+  display: flex;
+  gap: 5px;
+  padding: 8px 0;
 }
 .overall-comment,
 .inline-comment {
@@ -55,19 +54,20 @@ export default `
   gap: 0;
   justify-content: space-between;
 }
-.comment-header .text-h4 {
-  font-weight: bold;
+.inline-comment > .comment > .comment-header > div > .q-btn[aria-label="Go To Highlight"] {
+  cursor: pointer;
+  display: inline-block;
 }
-aside.q-drawer {
-  transform: none !important;
-  width: auto !important;
+.inline-comment > .comment > .comment-header > div > .q-btn[aria-label="Go To Highlight"] * {
+  pointer-events: none;
 }
-.comment-highlight {
-  background-color: #c9e5f8;
+.inline-comment > .comment > .comment-header > div > .q-btn[aria-label="Go To Highlight"] i {
+  font-size: 0 !important;
 }
-.style-criteria-section {
-  display: flex;
-  gap: 5px;
-  padding: 8px 0;
+.inline-comment > .comment > .comment-header > div > .q-btn[aria-label="Go To Highlight"] i:before {
+  content: "⇧";
+  display: inline-block;
+  font-size: 1rem;
+  font-style: normal;
 }
 `
