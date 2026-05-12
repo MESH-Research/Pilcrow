429928c11203b7d0d2037de93000ab1a11ff1c31
Grant Eben <gmeben@gmail.com>
2026-05-12 16:43:57 -0400

    fix: Resolve backend linting issue

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -5,7 +5,6 @@ namespace App\Models;
 
 use Carbon\Carbon;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
